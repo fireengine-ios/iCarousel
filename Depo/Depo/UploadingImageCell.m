1d9da7f569ec994345ef098b4e38dc4d35dae305
Mahir Tarlan <mahirtarlan@gmail.com>
2015-02-26 11:30:07 +0200

    thumbnail for queue

--- Depo/Depo/UploadingImageCell.m
+++ Depo/Depo/UploadingImageCell.m
@@ -28,10 +28,23 @@
 
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul), ^(void) {
             @autoreleasepool {
+                /*
                 UIImage *image = [[UIImage alloc] initWithContentsOfFile:self.uploadRef.tempThumbnailUrl];
                 dispatch_async(dispatch_get_main_queue(), ^{
                     imgView.image = image;
                 });
+                 */
+                //ALAssetsLibrary'den alir hale getirildi mahir-26.02.15
+                if(self.uploadRef.assetUrl) {
+                    NSURL *assetUrl = [NSURL URLWithString:self.uploadRef.assetUrl];
+                    ALAssetsLibrary *assetsLibraryForSingle = [[ALAssetsLibrary alloc] init];
+                    [assetsLibraryForSingle assetForURL:assetUrl resultBlock:^(ALAsset *myAsset) {
+                         CGImageRef thumbnailRef = [myAsset thumbnail];
+                         if (thumbnailRef) {
+                             imgView.image = [UIImage imageWithCGImage:thumbnailRef];
+                         }
+                     } failureBlock:nil];
+                }
             }
         });
 
