08418943eab95bfaf64ac7d90063f701083da507
Mahir K. Tarlan <mahirtarlan@gmail.com>
2015-05-20 16:27:45 +0300

    telefon şarjda iken sleep yapmama

--- Depo/Depo/AppDelegate.m
+++ Depo/Depo/AppDelegate.m
@@ -494,6 +494,7 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [UIApplication sharedApplication].idleTimerDisabled = NO;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -536,6 +537,11 @@
     } else {
         // eğer backgrounddan gelmiyorsa bir sonraki auto sync bloğununun okunmasını engelleyen lock kaldırılıyor
         [SyncUtil unlockAutoSyncBlockInProgress];
+    }
+
+    [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
+    if ([[UIDevice currentDevice] batteryState] == UIDeviceBatteryStateCharging || [[UIDevice currentDevice] batteryState] == UIDeviceBatteryStateFull) {
+        [UIApplication sharedApplication].idleTimerDisabled = YES;
     }
 }
 
