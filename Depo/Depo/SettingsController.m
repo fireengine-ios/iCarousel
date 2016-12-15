4908ba656ce3a875244d03dbca38940098fc29ca
Turan Yilmaz <turan.yilmaz@rdc.com.tr>
2016-12-15 08:50:37 +0300

    Test flight icin versiyon numaralari degistirildi ve Facebook import secenegi kaldirildi.

--- Depo/Depo/SettingsController.m
+++ Depo/Depo/SettingsController.m
@@ -330,7 +330,7 @@
 }
 
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 7;
+    return 6;
 }
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -398,15 +398,17 @@
         TitleCell *cell = [[TitleCell alloc] initWithCellStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier titleText:NSLocalizedString(@"ExportFromDropbox", @"") titleColor:nil subTitleText:@"" iconName:@"nav_download_icon" hasSeparator:drawSeparator isLink:YES linkText:@"" cellHeight:cellHeight];
         cell.backgroundView = [[UIView alloc] initWithFrame:cell.bounds];
         return cell;
-            } else if (indexPath.row == 5) {
-                TitleCell *cell = [[TitleCell alloc] initWithCellStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier titleText:NSLocalizedString(@"SocialMediaTitle", @"") titleColor:nil subTitleText:@"" iconName:@"icon_sm.png" hasSeparator:drawSeparator isLink:YES linkText:@"" cellHeight:cellHeight];
-                cell.backgroundView = [[UIView alloc] initWithFrame:cell.bounds];
-                return cell;
-    } else if (indexPath.row == 6) {
+            }
+//    else if (indexPath.row == 5) {
+//                TitleCell *cell = [[TitleCell alloc] initWithCellStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier titleText:NSLocalizedString(@"SocialMediaTitle", @"") titleColor:nil subTitleText:@"" iconName:@"icon_sm.png" hasSeparator:drawSeparator isLink:YES linkText:@"" cellHeight:cellHeight];
+//                cell.backgroundView = [[UIView alloc] initWithFrame:cell.bounds];
+//                return cell;
+//    }
+    else if (indexPath.row == 5) {
         TitleCell *cell = [[TitleCell alloc] initWithCellStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier titleText:NSLocalizedString(@"FAQ", @"") titleColor:nil subTitleText:@"" iconName:@"help_icon" hasSeparator:drawSeparator isLink:YES linkText:@"" cellHeight:cellHeight];
         cell.backgroundView = [[UIView alloc] initWithFrame:cell.bounds];
         return cell;
-    } else if (indexPath.row == 7) {
+    } else if (indexPath.row == 6) {
         TitleCell *cell = [[TitleCell alloc] initWithCellStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier titleText:@"Mail Logs" titleColor:nil subTitleText:@"" iconName:@"help_icon" hasSeparator:drawSeparator isLink:YES linkText:@"" cellHeight:cellHeight];
         cell.backgroundView = [[UIView alloc] initWithFrame:cell.bounds];
         return cell;
@@ -438,13 +440,13 @@
         case 4:
             [self didTriggerExportFromDropbox];
             break;
+//        case 5:
+//            [self didTriggerExportFromSocial];
+//            break;
         case 5:
-            [self didTriggerExportFromSocial];
-            break;
-        case 6:
             [self didTriggerHelp];
             break;
-        case 7:
+        case 6:
             [self triggerMailLog];
             break;
         default:
