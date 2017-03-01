c49bffe7edd8ea607c91b709dc1d6c571aac1767
Ömer Burak Kır <omerburakkir@gmail.com>
2017-03-01 16:04:38 +0300

    Cropy print

--- Depo/Depo/CropAndShareListController.m
+++ Depo/Depo/CropAndShareListController.m
@@ -296,7 +296,7 @@
                 [filteredPhotoList addObject:file];
             }
         }
-        ImagePreviewController *detail = [[ImagePreviewController alloc] initWithFiles:photoList withImage:fileSelected withListOffset:listOffset printEnabled:NO];
+        ImagePreviewController *detail = [[ImagePreviewController alloc] initWithFiles:photoList withImage:fileSelected withListOffset:listOffset printEnabled:YES];
         detail.delegate = self;
         MyNavigationController *modalNav = [[MyNavigationController alloc] initWithRootViewController:detail];
         detail.nav = modalNav;
