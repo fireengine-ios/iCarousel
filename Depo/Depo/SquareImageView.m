bede9026607677725583a2e0e1714b986e749005
Ömer Burak Kır <omer.kir@rdc.com.tr>
2017-03-17 14:58:30 +0300

    Fabric SquareImageView.m line 225 SquareImageView.m line 213

--- Depo/Depo/SquareImageView.m
+++ Depo/Depo/SquareImageView.m
@@ -22,7 +22,6 @@
 @implementation SquareImageView
 
 @synthesize delegate;
-@synthesize imgView;
 @synthesize file;
 @synthesize uploadRef;
 @synthesize longPressGesture;
@@ -47,19 +46,19 @@
         self.backgroundColor = [Util UIColorForHexColor:@"E3E3E3"];
         isSelectible = selectibleStatus;
 
-        imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-        imgView.contentMode = UIViewContentModeScaleAspectFill;
-        imgView.clipsToBounds = YES;
+        self.imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        self.imgView.contentMode = UIViewContentModeScaleAspectFill;
+        self.imgView.clipsToBounds = YES;
         if(cacheFlag) {
-            [imgView setImageWithURL:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil];
+            [self.imgView setImageWithURL:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil];
         } else {
             if(manualQuality) {
-                [imgView setNoCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil];
+                [self.imgView setNoCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil];
             } else {
-                [imgView setNoCachedImageWithURL:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil];
+                [self.imgView setNoCachedImageWithURL:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil];
             }
         }
-        [self addSubview:imgView];
+        [self addSubview:self.imgView];
         
         if(self.file.contentType == ContentTypeVideo) {
             UIImageView *playIconView = [[UIImageView alloc] initWithFrame:CGRectMake(4, self.frame.size.height - 22, 18, 18)];
@@ -102,11 +101,11 @@
         if(IS_IPAD) {
             imgMaxWidth = 600;
         }
-        imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-        imgView.contentMode = UIViewContentModeScaleAspectFill;
-        imgView.clipsToBounds = YES;
-        [imgView setFinalNoCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil withMaxWidth:imgMaxWidth withMaxHeight:imgMaxWidth forCompressQaulity:0.8f];
-        [self addSubview:imgView];
+        self.imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        self.imgView.contentMode = UIViewContentModeScaleAspectFill;
+        self.imgView.clipsToBounds = YES;
+        [self.imgView setFinalNoCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil withMaxWidth:imgMaxWidth withMaxHeight:imgMaxWidth forCompressQaulity:0.8f];
+        [self addSubview:self.imgView];
         
         if(self.file.contentType == ContentTypeVideo) {
             playIconView = [[UIImageView alloc] initWithFrame:CGRectMake(4, self.frame.size.height - 22, 18, 18)];
@@ -144,11 +143,11 @@
         if(IS_IPAD) {
             imgMaxWidth = 600;
         }
-        imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-        imgView.contentMode = UIViewContentModeScaleAspectFill;
-        imgView.clipsToBounds = YES;
-        [imgView setFinalCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil withMaxWidth:imgMaxWidth withMaxHeight:imgMaxWidth forCompressQaulity:0.8f];
-        [self addSubview:imgView];
+        self.imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        self.imgView.contentMode = UIViewContentModeScaleAspectFill;
+        self.imgView.clipsToBounds = YES;
+        [self.imgView setFinalCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil withMaxWidth:imgMaxWidth withMaxHeight:imgMaxWidth forCompressQaulity:0.8f];
+        [self addSubview:self.imgView];
         
         if(self.file.contentType == ContentTypeVideo) {
             playIconView = [[UIImageView alloc] initWithFrame:CGRectMake(4, self.frame.size.height - 22, 18, 18)];
@@ -182,7 +181,7 @@
     if(IS_IPAD) {
         imgMaxWidth = 600;
     }
-    [imgView setFinalCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil withMaxWidth:imgMaxWidth withMaxHeight:imgMaxWidth forCompressQaulity:0.8f];
+    [self.imgView setFinalCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil withMaxWidth:imgMaxWidth withMaxHeight:imgMaxWidth forCompressQaulity:0.8f];
 
     if(self.file.contentType == ContentTypeVideo) {
         if(!playIconView) {
@@ -212,19 +211,19 @@
         self.uploadRef = ref;
         self.backgroundColor = [Util UIColorForHexColor:@"E3E3E3"];
         
-        imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-        imgView.contentMode = UIViewContentModeScaleAspectFill;
-        imgView.clipsToBounds = YES;
-        imgView.alpha = 0.5f;
+        self.imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        self.imgView.contentMode = UIViewContentModeScaleAspectFill;
+        self.imgView.clipsToBounds = YES;
+        self.imgView.alpha = 0.5f;
 //        imgView.image = [UIImage imageNamed:@"square_placeholder.png"];
-        [self addSubview:imgView];
+        [self addSubview:self.imgView];
 
         [self loadUploadRef];
         
-        progressSeparator = [[UIView alloc] initWithFrame:CGRectMake(0, self.frame.size.height-6, 1, 6)];
-        progressSeparator.backgroundColor = [Util UIColorForHexColor:@"3fb0e8"];
-        progressSeparator.alpha = 0.75f;
-        [self addSubview:progressSeparator];
+        self.progressSeparator = [[UIView alloc] initWithFrame:CGRectMake(0, self.frame.size.height-6, 1, 6)];
+        self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"3fb0e8"];
+        self.progressSeparator.alpha = 0.75f;
+        [self addSubview:self.progressSeparator];
 
         maskView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
         maskView.image = [UIImage imageNamed:@"selected_mask.png"];
@@ -264,7 +263,7 @@
                     if(myAsset) {
                         CGImageRef thumbnailRef = [myAsset thumbnail];
                         if (thumbnailRef) {
-                            imgView.image = [UIImage imageWithCGImage:thumbnailRef];
+                            self.imgView.image = [UIImage imageWithCGImage:thumbnailRef];
                         }
                     } else {
                         [assetsLibraryForSingle enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
@@ -272,7 +271,7 @@
                                 if([[result.defaultRepresentation.url absoluteString] isEqualToString:self.uploadRef.assetUrl]) {
                                     CGImageRef thumbnailRef = [result thumbnail];
                                     if (thumbnailRef) {
-                                        imgView.image = [UIImage imageWithCGImage:thumbnailRef];
+                                        self.imgView.image = [UIImage imageWithCGImage:thumbnailRef];
                                     }
                                 }
                             }];
@@ -282,7 +281,7 @@
             }
         } else if(self.uploadRef.taskType == UploadTaskTypeFile) {
             UIImage *thumbImageFromCam = [UIImage imageWithContentsOfFile:self.uploadRef.tempUrl];
-            imgView.image = [Util imageWithImage:thumbImageFromCam scaledToFillSize:CGSizeMake(40, 40)];
+            self.imgView.image = [Util imageWithImage:thumbImageFromCam scaledToFillSize:CGSizeMake(40, 40)];
         }
     }
     NSMutableArray *managersArray = [[UploadQueue sharedInstance].uploadManagers copy];
@@ -316,7 +315,7 @@
              */
             
             UIImage *image = [[UIImage alloc] initWithContentsOfFile:self.uploadRef.tempThumbnailUrl];
-            imgView.image = image;
+            self.imgView.image = image;
         }
     }
 }
@@ -351,7 +350,7 @@
     if(maskView) {
         maskView.hidden = YES;
     }
-    imgView.alpha = 0.25f;
+    self.imgView.alpha = 0.25f;
 }
 
 - (void) old_touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -435,12 +434,12 @@
 
 - (void) recheckAndDrawProgress {
     if(self.uploadRef.hasFinished) {
-        imgView.alpha = 1.0f;
-        progressSeparator.frame = CGRectMake(0, self.frame.size.height-6, self.frame.size.width, 6);
+        self.imgView.alpha = 1.0f;
+        self.progressSeparator.frame = CGRectMake(0, self.frame.size.height-6, self.frame.size.width, 6);
         if(self.uploadRef.hasFinishedWithError) {
-            progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
+            self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
         } else {
-            progressSeparator.backgroundColor = [Util UIColorForHexColor:@"67d74b"];
+            self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"67d74b"];
         }
     }
 }
@@ -451,32 +450,32 @@
 }
 
 - (void) updateProgressByWidth:(NSNumber *) newWidth {
-    progressSeparator.frame = CGRectMake(0, self.frame.size.height-6, [newWidth intValue], 6);
+    self.progressSeparator.frame = CGRectMake(0, self.frame.size.height-6, [newWidth intValue], 6);
 }
 
 - (void) uploadManagerDidFailUploadingForAsset:(NSString *) assetToUpload {
-    progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
+    self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
 }
 
 - (void) uploadManagerQuotaExceedForAsset:(NSString *) assetToUpload {
     uploadErrorType = UploadErrorTypeQuota;
 
     [self updateProgressByWidth:[NSNumber numberWithLong:self.frame.size.width]];
-    progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
+    self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
 }
 
 - (void) uploadManagerLoginRequiredForAsset:(NSString *) assetToUpload {
     uploadErrorType = UploadErrorTypeLogin;
 
     [self updateProgressByWidth:[NSNumber numberWithLong:self.frame.size.width]];
-    progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
+    self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
 }
 
 - (void) uploadManagerDidFinishUploadingForAsset:(NSString *)assetToUpload withFinalFile:(MetaFile *) finalFile {
     self.file = finalFile;
     [self updateProgressByWidth:[NSNumber numberWithLong:self.frame.size.width]];
-    progressSeparator.backgroundColor = [Util UIColorForHexColor:@"67d74b"];
-    imgView.alpha = 1.0f;
+    self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"67d74b"];
+    self.imgView.alpha = 1.0f;
     if([delegate respondsToSelector:@selector(squareImageUploadFinishedForFile:)]) {
         [delegate squareImageUploadFinishedForFile:self.uploadRef.fileUuid];
     }
@@ -484,12 +483,12 @@
 
 - (void) uploadManagerDidFailUploadingAsData {
     [self updateProgressByWidth:[NSNumber numberWithLong:self.frame.size.width]];
-    progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
+    self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"ad3110"];
 }
 
 - (void) uploadManagerDidFinishUploadingAsData {
     [self updateProgressByWidth:[NSNumber numberWithLong:self.frame.size.width]];
-    progressSeparator.backgroundColor = [Util UIColorForHexColor:@"67d74b"];
+    self.progressSeparator.backgroundColor = [Util UIColorForHexColor:@"67d74b"];
 }
 
 - (void) dealloc {
@@ -506,9 +505,9 @@
 */
 
 - (void) unloadContent {
-    if(imgView) {
-        [imgView removeFromSuperview];
-        imgView = nil;
+    if(self.imgView) {
+        [self.imgView removeFromSuperview];
+        self.imgView = nil;
     }
     if(maskView) {
         [maskView removeFromSuperview];
@@ -520,12 +519,12 @@
 - (void) reloadContent {
     if(wasUnloaded) {
         wasUnloaded = NO;
-        if(!imgView) {
-            imgView = [[UIImageView alloc] initWithFrame:CGRectMake(-1, -1, self.frame.size.width+2, self.frame.size.height+2)];
-            imgView.contentMode = UIViewContentModeScaleAspectFill;
-            imgView.clipsToBounds = YES;
-            [imgView setFinalNoCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil withMaxWidth:200 withMaxHeight:200 forCompressQaulity:0.8f];
-            [self addSubview:imgView];
+        if(!self.imgView) {
+            self.imgView = [[UIImageView alloc] initWithFrame:CGRectMake(-1, -1, self.frame.size.width+2, self.frame.size.height+2)];
+            self.imgView.contentMode = UIViewContentModeScaleAspectFill;
+            self.imgView.clipsToBounds = YES;
+            [self.imgView setFinalNoCachedImageWithBetterQualityForUrl:[NSURL URLWithString:[self.file.detail.thumbMediumUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:nil withMaxWidth:200 withMaxHeight:200 forCompressQaulity:0.8f];
+            [self addSubview:self.imgView];
         }
     }
 }
