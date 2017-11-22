8d0785a52a6d2dbe768df3d4d74be32c6ca0056d
Roman Harhun <raman.harhun@consultant.turkcell.com.tr>
2017-11-22 14:23:24 +0300

    Fix: Unynced Photo preview - Edit button is displayed

--- Depo/Depo/Classes/Modules/PhotoVideoDetail/View/PhotoVideoDetailViewController.swift
+++ Depo/Depo/Classes/Modules/PhotoVideoDetail/View/PhotoVideoDetailViewController.swift
@@ -20,7 +20,12 @@ class PhotoVideoDetailViewController: BaseViewController, PhotoVideoDetailViewIn
     var output: PhotoVideoDetailViewOutput!
     var interactor: PhotoVideoDetailInteractor?
     var views: [BaseFileContentView] = [BaseFileContentView]()
-    var selectedIndex: Int = -1 { didSet { configureNavigationBar() } }
+    var selectedIndex: Int = -1 {
+        didSet {
+            configureNavigationBar()
+            configureEditingTabBar()
+        }
+    }
     var isAnimating = false
     var objects = [Item]() { didSet { configureNavigationBar() } }
     let customPopUp = CustomPopUp()
@@ -67,6 +72,19 @@ class PhotoVideoDetailViewController: BaseViewController, PhotoVideoDetailViewIn
         if objects.count > selectedIndex, selectedIndex >= 0 {
             let item = objects[selectedIndex]
             navigationItem.rightBarButtonItem?.customView?.isHidden = item.syncStatus == .notSynced
+        }
+    }
+    
+    private func configureEditingTabBar() {
+        if objects.count > selectedIndex, selectedIndex >= 0,
+           let editIndex = interactor?.bottomBarOriginalConfig.elementsConfig.index(of: .edit) {
+            let item = objects[selectedIndex]
+            
+            if item.syncStatus == .notSynced {
+                editingTabBar.disableItems(atIntdex: [editIndex])
+            } else {
+                editingTabBar.enableIems(atIndex: [editIndex])
+            }
         }
     }
     
