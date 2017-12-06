7c9a72332b1de9e98d6ed96e5214caebe784c8dd
OlegRidevskij <oleg@MacBook-Pro-Oleg.local>
2017-12-06 14:09:17 +0300

    fix

--- Depo/Depo/Presentation/UserStories/Grid/UploadFromLifeBox/Interactor/UploadFromLifeBoxInteractor.swift
+++ Depo/Depo/Presentation/UserStories/Grid/UploadFromLifeBox/Interactor/UploadFromLifeBoxInteractor.swift
@@ -15,34 +15,25 @@ class UploadFromLifeBoxInteractor: BaseFilesGreedInteractor, UploadFromLifeBoxIn
         if router.isRootViewControllerAlbumDetail(){
             let parameter = AddPhotosToAlbum(albumUUID: rootFolderUUID, photos: items)
             PhotosAlbumService().addPhotosToAlbum(parameters: parameter, success: { [weak self] in
-                if let self_ = self {
-                    self_.output.asyncOperationSucces()
-                    
-                    guard let out = self_.output as? UploadFromLifeBoxInteractorOutput else{
-                        return
-                    }
-                    out.uploadOperationSuccess()
+                self?.output.asyncOperationSucces()
+                guard let out = self?.output as? UploadFromLifeBoxInteractorOutput else{
+                    return
                 }
+                out.uploadOperationSuccess()
             }, fail: { [weak self] (error) in
-                if let self_ = self {
-                    self_.output.asyncOperationFail(errorMessage: TextConstants.failWhileAddingToAlbum)
-                }
+                self?.output.asyncOperationFail(errorMessage: TextConstants.failWhileAddingToAlbum)
             })
         }else{
             let itemsUUIDs = items.map({ $0.uuid })
             let parametr = CopyFiles(items: itemsUUIDs, path: rootFolderUUID)
             FileService().copy(copyparam: parametr, success: { [weak self] in
-                if let self_ = self {
-                    self_.output.asyncOperationSucces()
-                    guard let out = self_.output as? UploadFromLifeBoxInteractorOutput else{
-                        return
-                    }
-                    out.uploadOperationSuccess()
+                self?.output.asyncOperationSucces()
+                guard let out = self?.output as? UploadFromLifeBoxInteractorOutput else{
+                    return
                 }
+                out.uploadOperationSuccess()
             }, fail: { [weak self] (fail) in
-                if let self_ = self {
-                    self_.output.asyncOperationFail(errorMessage: TextConstants.failWhileuploadFromLifeBoxCopy)
-                }
+                self?.output.asyncOperationFail(errorMessage: TextConstants.failWhileuploadFromLifeBoxCopy)
             })
         }
     }
