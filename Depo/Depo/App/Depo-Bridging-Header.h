e2cdfb05d6cf63a06472f1dd094a5a4964c71a37
AleksandrP <Aleksandr.Pestriakov@life.com.by>
2017-12-07 15:09:51 +0300

    removed facebook and carousel from bridging header

--- Depo/Depo/App/Depo-Bridging-Header.h
+++ Depo/Depo/App/Depo-Bridging-Header.h
@@ -5,8 +5,8 @@
 #import "ContactSyncSDK.h"
 #import "SyncSettings.h"
 #import "SyncStatus.h"
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <iCarousel/iCarousel.h>
+//#import <FBSDKCoreKit/FBSDKCoreKit.h>
+//#import <iCarousel/iCarousel.h>
 #import "AppRater.h"
 #import "MD5.h"
 #import "MPush.h"
