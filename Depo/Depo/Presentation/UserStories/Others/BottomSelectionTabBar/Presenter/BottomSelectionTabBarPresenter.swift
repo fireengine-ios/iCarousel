26e696979cf16c8eebbd87a6fe91eb6147ec95d1
Tsimafei.Harhun <timgorgun@gmail.ru>
2017-12-13 12:11:23 +0300

    LR-1316 Select mode - Select mode is shown after actions with selected files

--- Depo/Depo/Presentation/UserStories/Others/BottomSelectionTabBar/Presenter/BottomSelectionTabBarPresenter.swift
+++ Depo/Depo/Presentation/UserStories/Others/BottomSelectionTabBar/Presenter/BottomSelectionTabBarPresenter.swift
@@ -133,6 +133,7 @@ class BottomSelectionTabBarPresenter: MoreFilesActionsPresenter, BottomSelection
             interactor.delete(item: selectedItems)
             basePassingPresenter?.stopModeSelected()
         case .download:
+            basePassingPresenter?.stopModeSelected()
             interactor.download(item: selectedItems)
         case .edit:
             interactor.edit(item: selectedItems)
@@ -158,6 +159,7 @@ class BottomSelectionTabBarPresenter: MoreFilesActionsPresenter, BottomSelection
             }
             basePassingPresenter?.stopModeSelected()
         case .sync:
+            basePassingPresenter?.stopModeSelected()
             interactor.sync(item: selectedItems)
         case .removeFromAlbum:
             interactor.removeFromAlbum(items: selectedItems)
