d5de4d7d886d7ee99f81fe06e604738f43057b6b
Tsimafei.Harhun <timgorgun@gmail.ru>
2017-12-15 11:16:17 +0300

    LR-2698 Upload - Screen with albums existing in device gallery is absent

--- Depo/Depo/Support/Extensions/PHAssetCollection.swift
+++ Depo/Depo/Support/Extensions/PHAssetCollection.swift
@@ -21,6 +21,10 @@ extension PHAssetCollection {
     }
     
     var videosCount: Int {
+        guard LocalMediaStorage.default.photoLibraryIsAvailible() else {
+            return 0
+        }
+        
         let fetchOptions = PHFetchOptions()
         fetchOptions.predicate = NSPredicate(format: "mediaType == %d", PHAssetMediaType.video.rawValue)
         let result = PHAsset.fetchAssets(in: self, options: fetchOptions)
