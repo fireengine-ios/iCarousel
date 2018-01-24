d38a6640eb0289323648d3d4dd3b76d5c45e188e
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2018-01-24 15:22:06 +0300

    created FilterPhotoCard

---
+++ Depo/Depo/Presentation/SubViews/ViewForPopUp/PopUpSubViews/FilterPhotoCard/FilterPhotoCard.swift
@@ -0,0 +1,32 @@
+//
+//  FilterPhotoCard.swift
+//  Depo
+//
+//  Created by Bondar Yaroslav on 1/24/18.
+//  Copyright © 2018 LifeTech. All rights reserved.
+//
+
+import UIKit
+//ACEDrawingView
+final class FilterPhotoCard: BaseView {
+    
+//    @IBOutlet weak var titleLabel: UILabel?
+//    @IBOutlet weak var settingsButton: CircleYellowButton?
+//
+//    override func configurateView() {
+//        super.configurateView()
+//
+//        titleLabel?.text = TextConstants.waitingForWiFiPopUpTitle
+//        titleLabel?.font = UIFont.TurkcellSaturaRegFont(size: 18)
+//        titleLabel?.textColor = ColorConstants.textGrayColor
+//
+//        settingsButton?.setTitle(TextConstants.waitingForWiFiPopUpSettingsButton, for: .normal)
+//    }
+//
+//    @IBAction func onSettingsButton(){
+//        let router = RouterVC()
+//        router.pushViewController(viewController: router.autoUpload)
+//    }
+    
+}
+
