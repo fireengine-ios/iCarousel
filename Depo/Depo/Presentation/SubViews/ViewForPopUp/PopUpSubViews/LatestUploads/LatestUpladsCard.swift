f633e1f4a551a0272ef3501b8fb5a9d7a744d73f
OlegRidevskij <oleg@MacBook-Pro-Oleg.local>
2018-01-27 17:10:46 +0300

    dded latestUpload card

---
+++ Depo/Depo/Presentation/SubViews/ViewForPopUp/PopUpSubViews/LatestUploads/LatestUpladsCard.swift
@@ -0,0 +1,15 @@
+//
+//  LatestUpladsCard.swift
+//  Depo
+//
+//  Created by Oleg on 27.01.2018.
+//  Copyright © 2018 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+class LatestUpladsCard: UIView {
+
+    
+
+}
