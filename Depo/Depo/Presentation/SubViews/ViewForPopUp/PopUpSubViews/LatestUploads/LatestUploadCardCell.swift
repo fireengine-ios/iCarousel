354f12458cd0f7db7d4e17c33434c59fab71b1b3
OlegRidevskij <oleg@MacBook-Pro-Oleg.local>
2018-01-29 16:45:44 +0300

    update view

---
+++ Depo/Depo/Presentation/SubViews/ViewForPopUp/PopUpSubViews/LatestUploads/LatestUploadCardCell.swift
@@ -0,0 +1,18 @@
+//
+//  LatestUploadCardCell.swift
+//  Depo
+//
+//  Created by Oleg on 29.01.2018.
+//  Copyright © 2018 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+class LatestUploadCardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
