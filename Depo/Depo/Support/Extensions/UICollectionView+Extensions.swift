a105ffb39b488cf69d1f890e2e3d9ad446ca908f
Konstantin Studilin <konstantin.studilin@life.com.by>
2018-01-29 15:18:53 +0300

    force unwrapping fix

--- Depo/Depo/Support/Extensions/UICollectionView+Extensions.swift
+++ Depo/Depo/Support/Extensions/UICollectionView+Extensions.swift
@@ -39,7 +39,9 @@ extension UICollectionView {
 
 extension UICollectionView {
     func indexPathsForElements(in rect: CGRect) -> [IndexPath] {
-        let allLayoutAttributes = collectionViewLayout.layoutAttributesForElements(in: rect)!
-        return allLayoutAttributes.map { $0.indexPath }
+        if let allLayoutAttributes = collectionViewLayout.layoutAttributesForElements(in: rect) {
+            return allLayoutAttributes.map { $0.indexPath }
+        }
+        return []
     }
 }
