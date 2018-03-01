ba7cbd88d40ccf207674856e268427865bfdca51
Selim Savsar <selim@Selims-iMac.local>
2018-03-01 15:04:46 +0300

    Fixed according to comments on Pull Request

--- Depo/Depo/Business/Models/ServerStatusError.swift
+++ Depo/Depo/Business/Models/ServerStatusError.swift
@@ -85,8 +85,7 @@ extension ServerStatusError: LocalizedError {
             return TextConstants.EMAIL_IS_INVALID
             
         case ErrorKeys.EMAIL_IS_ALREADY_EXIST:
-            return TextConstants.errorExistEmail
-            //return TextConstants.EMAIL_IS_ALREADY_EXIST
+            return TextConstants.EMAIL_IS_ALREADY_EXIST
             
         default:
             return status
