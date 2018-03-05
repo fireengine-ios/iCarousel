27d5edb4480b91afddf31d5cee116885838e8b92
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2018-03-05 17:23:44 +0300

    created waitFilePreparation at url

--- Depo/LifeboxShared/Business/UploadService.swift
+++ Depo/LifeboxShared/Business/UploadService.swift
@@ -38,8 +38,27 @@ final class UploadService {
         }
     }
     
-    func upload(url: URL, contentType: String, progressHandler: @escaping Request.ProgressHandler, dataRequestHandler: DataRequestHandler?, complition: @escaping ResponseVoid) {
+    private func waitFilePreparation(at url: URL, complition: ResponseVoid) {
+        var fcError: NSError?
+        let fileCoordinator = NSFileCoordinator()
         
+        fileCoordinator.coordinate(readingItemAt: url, options: .forUploading, error: &fcError) { (readUrl) in
+            if let error = fcError {
+                complition(ResponseResult.failed(error))
+            } else {
+                complition(ResponseResult.success(()))
+            }
+        }
+    }
+    
+    
+//    do {
+//    try waitFilePreparation(at: url)
+//    } catch  {
+//    complition(ResponseResult.failed(error))
+//    return
+//    }
+    private func waitFilePreparation(at url: URL) throws {
         var fcError: NSError?
         let fileCoordinator = NSFileCoordinator()
         let semaphore = DispatchSemaphore(value: 0)
@@ -50,51 +69,58 @@ final class UploadService {
         semaphore.wait()
         
         if let error = fcError {
-            complition(ResponseResult.failed(error))
-            return
+            throw error
         }
+    }
+    
+    func upload(url: URL, contentType: String, progressHandler: @escaping Request.ProgressHandler, dataRequestHandler: DataRequestHandler?, complition: @escaping ResponseVoid) {
         
-        
-        
-        
-        let dataRequest = getBaseUploadUrl { [weak self] result in
-            
+        waitFilePreparation(at: url) { [weak self] result in
             switch result {
-            case .success(let path):
+            case .success(_):
                 
-                guard let `self` = self else {
-                    return
-                }
-                
-                let uploadUrl = path + "/" + UUID().uuidString
-                
-                let headers: HTTPHeaders = [
-                    HeaderConstant.XObjectMetaFavorites: "false",
-                    HeaderConstant.XMetaStrategy: "1",
-                    HeaderConstant.Expect: "100-continue",
-                    HeaderConstant.XObjectMetaParentUuid: "",
-                    HeaderConstant.XObjectMetaFileName: url.lastPathComponent,
-                    HeaderConstant.ContentType: contentType,
-                    HeaderConstant.XObjectMetaSpecialFolder: "MOBILE_UPLOAD"
-                ]
-                
-                let dataRequest = self.sessionManager
-                    .upload(url, to: uploadUrl, method: .put, headers: headers)
-                    .customValidate()
-                    .uploadProgress(closure: progressHandler)
-                    .responseString { response in
-                        switch response.result {
-                        case .success(_):
-                            complition(ResponseResult.success(()))
-                        case .failure(let error):
-                            complition(ResponseResult.failed(error))
+                let dataRequest = getBaseUploadUrl { [weak self] result in
+                    switch result {
+                    case .success(let path):
+                        
+                        guard let `self` = self else {
+                            return
                         }
+                        
+                        let uploadUrl = path + "/" + UUID().uuidString
+                        
+                        let headers: HTTPHeaders = [
+                            HeaderConstant.XObjectMetaFavorites: "false",
+                            HeaderConstant.XMetaStrategy: "1",
+                            HeaderConstant.Expect: "100-continue",
+                            HeaderConstant.XObjectMetaParentUuid: "",
+                            HeaderConstant.XObjectMetaFileName: url.lastPathComponent,
+                            HeaderConstant.ContentType: contentType,
+                            HeaderConstant.XObjectMetaSpecialFolder: "MOBILE_UPLOAD"
+                        ]
+                        
+                        let dataRequest = self.sessionManager
+                            .upload(url, to: uploadUrl, method: .put, headers: headers)
+                            .customValidate()
+                            .uploadProgress(closure: progressHandler)
+                            .responseString { response in
+                                switch response.result {
+                                case .success(_):
+                                    complition(ResponseResult.success(()))
+                                case .failure(let error):
+                                    complition(ResponseResult.failed(error))
+                                }
+                        }
+                        dataRequestHandler?(dataRequest)
+                    case .failed(let error):
+                        complition(ResponseResult.failed(error))
+                    }
                 }
                 dataRequestHandler?(dataRequest)
+                
             case .failed(let error):
                 complition(ResponseResult.failed(error))
             }
         }
-        dataRequestHandler?(dataRequest)
     }
 }
