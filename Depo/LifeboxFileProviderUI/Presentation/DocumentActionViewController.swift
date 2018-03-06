2eb19a876f9429d021d0ab80f75c5f6ab2dc7324
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2018-03-06 11:27:51 +0300

    clear code

--- Depo/LifeboxFileProviderUI/Presentation/DocumentActionViewController.swift
+++ Depo/LifeboxFileProviderUI/Presentation/DocumentActionViewController.swift
@@ -16,12 +16,6 @@ final class DocumentActionViewController: FPUIActionExtensionViewController {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var messageLabel: UILabel!
     
-    /// not used
-//    override func prepare(forAction actionIdentifier: String, itemIdentifiers: [NSFileProviderItemIdentifier]) {
-//        identifierLabel?.text = actionIdentifier
-//        actionTypeLabel?.text = "Custom action"
-//    }
-    
     override func prepare(forError error: Error) {
         if error.localizedDescription == "passcode" {
             titleLabel?.text = L10n.errorPasscodeTitle
@@ -34,14 +28,7 @@ final class DocumentActionViewController: FPUIActionExtensionViewController {
             messageLabel?.text = error.localizedDescription
         }
     }
-
-    /// not used
-//    @IBAction func doneButtonTapped(_ sender: Any) {
-//        // Perform the action and call the completion block. If an unrecoverable error occurs you must still call the completion block with an error. Use the error code FPUIExtensionErrorCode.failed to signal the failure.
-//        extensionContext.completeRequest()
-//    }
     
-    /// need
     @IBAction func cancelButtonTapped(_ sender: Any) {
         extensionContext.cancelRequest(withError: NSError(domain: FPUIErrorDomain, code: Int(FPUIExtensionErrorCode.userCancelled.rawValue), userInfo: nil))
     }
