03b602a3eaa6749ee8b223c9ede07bd589f17324
Tsimafei.Harhun <timgorgun@gmail.ru>
2018-03-13 13:18:27 +0300

    LR-4796 Places album - App crash when tap on empty album

---
+++ Depo/Depo/Presentation/UserStories/Grid/FaceImageItems/View/FaceImageItemsViewInput.swift
@@ -0,0 +1,13 @@
+//
+//  FaceImageItemsViewInput.swift
+//  Depo
+//
+//  Created by Harbros on 13.03.2018.
+//  Copyright © 2018 LifeTech. All rights reserved.
+//
+
+protocol FaceImageItemsViewInput: class {
+    func configurateUgglaView()
+    func updateUgglaViewPosition()
+    func showNoFilesWith(text: String, image: UIImage, createFilesButtonText: String, needHideTopBar: Bool, isShowUggla: Bool)
+}
