af8a0617cfd321add84974b341d883b3e7f03820
Andrei Novikov <anovikov.brest@gmail.com>
2018-03-15 16:08:34 +0300

    LR-4808 iOS: Free Up Space and Passcode Interaction

--- Depo/Depo/Presentation/UserStories/Settings/Passcode/PasscodeEnter/View/Passcode/PasscodeStorage.swift
+++ Depo/Depo/Presentation/UserStories/Settings/Passcode/PasscodeEnter/View/Passcode/PasscodeStorage.swift
@@ -12,6 +12,7 @@ public typealias Passcode = String
 
 protocol PasscodeStorage: class {
     var isEmpty: Bool { get }
+    var systemCallOnScreen: Bool { get set }
     func isEqual(to passcode: Passcode) -> Bool
     func save(passcode: Passcode)
     func clearPasscode()
@@ -35,6 +36,12 @@ final class PasscodeStorageDefaults {
     var numberOfTries: Int {
         get { return defaults?.integer(forKey: PasscodeStorageDefaults.numberOfTriesKey) ?? 0 }
         set { defaults?.set(newValue, forKey: PasscodeStorageDefaults.numberOfTriesKey) }
+    }
+    
+    static let systemCallOnScreen = "systemCallOnScreen"
+    var systemCallOnScreen: Bool {
+        get { return defaults?.bool(forKey: PasscodeStorageDefaults.systemCallOnScreen) ?? false }
+        set { defaults?.set(newValue, forKey: PasscodeStorageDefaults.systemCallOnScreen) }
     }
 }
 extension PasscodeStorageDefaults: PasscodeStorage {
