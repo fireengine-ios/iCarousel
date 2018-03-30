d93a848df652eb2a263dd98c98deafca9f7e35a7
Selim Savsar <selim@Selims-iMac.local>
2018-03-30 11:45:48 +0300

    Added Active Compilation Conditions to extensions

--- Depo/Shared/SharedConstants.swift
+++ Depo/Shared/SharedConstants.swift
@@ -12,7 +12,20 @@ import Foundation
 struct SharedConstants {
     private init() {}
     
+    #if APPSTORE
+        static let groupIdentifier = "group.com.turkcell.akillidepo"
+    #endif
+    #if ENTERPRISE
+    static let groupIdentifier = "group.com.turkcell.akillideponew.ent"
+    #endif
+    #if DEBUG
     static let groupIdentifier = "group.come.life.Lifebox"
+    #endif
+    #if RELEASE
+    static let groupIdentifier = "group.come.life.Lifebox"
+    #endif
+    
+    //static let groupIdentifier = "group.come.life.Lifebox"
     
     static let wormholeDirectoryIdentifier = "wormhole"
     static let wormholeMessageIdentifier = "wormholeMessageIdentifier"
