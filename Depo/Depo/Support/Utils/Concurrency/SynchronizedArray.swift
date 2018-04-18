a14be0e8340b58f4b9012fdd42092d1397472513
AleksandrP <Aleksandr.Pestriakov@life.com.by>
2018-04-18 21:26:45 +0300

    sync array should be with serial queue

--- Depo/Depo/Support/Utils/Concurrency/SynchronizedArray.swift
+++ Depo/Depo/Support/Utils/Concurrency/SynchronizedArray.swift
@@ -14,7 +14,7 @@ import Foundation
 
 /// A thread-safe array.
 public class SynchronizedArray<Element> {
-    fileprivate let queue = DispatchQueue(label: DispatchQueueLabels.syncronizedArray, attributes: .concurrent)
+    fileprivate let queue = DispatchQueue(label: DispatchQueueLabels.syncronizedArray)
     fileprivate var array = [Element]()
 }
 
