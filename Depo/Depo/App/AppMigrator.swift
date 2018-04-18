bbfa67dcbd9ad192058922924662586e9ec514f5
OlegRidevskij <oleg@MacBook-Pro-Oleg.local>
2018-04-18 16:32:29 +0300

    update

--- Depo/Depo/App/AppMigrator.swift
+++ Depo/Depo/App/AppMigrator.swift
@@ -13,20 +13,15 @@ class AppMigrator {
     private static let migrationHasBeenCompleted = "application2018migrationKey"
     
     class func migrateFromOldApplicationIfNeed() {
-        let userDefault = UserDefaults.standard
-        let needTryToMigrate = !userDefault.bool(forKey: migrationHasBeenCompleted)
+        
+        let needTryToMigrate = !UserDefaults.standard.bool(forKey: migrationHasBeenCompleted)
         
         if needTryToMigrate {
-            userDefault.set(true, forKey: migrationHasBeenCompleted)
+            //UserDefaults.standard.set(true, forKey: migrationHasBeenCompleted)
             
-            var refreshToken = ""
-            if let token = UserDefaults.standard.object(forKey: "REMEMBER_ME_TOKEN_KEY") as? String{
-                refreshToken = token
-            }
-            
-            if !refreshToken.isEmpty{
+            if let token = UserDefaults.standard.object(forKey: "REMEMBER_ME_TOKEN_KEY") as? String, !token.isEmpty{
                 let tokenStorage: TokenStorage = factory.resolve()
-                tokenStorage.refreshToken = refreshToken
+                tokenStorage.refreshToken = token
                 tokenStorage.isRememberMe = true
             }
         }
