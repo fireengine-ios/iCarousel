34a80ed8e579ee4dcebd4d0e92f76d42167a46ac
Andrei Novikov <anovikov.brest@gmail.com>
2018-04-20 16:59:38 +0300

    Logs size limit

--- Depo/Depo/App/Constants/NumericConstants.swift
+++ Depo/Depo/App/Constants/NumericConstants.swift
@@ -77,7 +77,7 @@ struct NumericConstants {
     static let emptyEmailUserCloseLimit = 3
     
     static let logDuration: TimeInterval = 24 * 60 * 60 * 3
-    static let logMaxSize: UInt64 = 10_485_760
+    static let logMaxSize: UInt64 = 5_242_880
     
     static let numberOfLocalItemsOnPage: Int = 100
     
