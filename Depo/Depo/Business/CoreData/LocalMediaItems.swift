677224f0b166ae93da7666159af5878912a83bfa
AleksandrP <Aleksandr.Pestriakov@life.com.by>
2018-04-23 12:14:44 +0300

    should be more operative now when added new photo/video

--- Depo/Depo/Business/CoreData/LocalMediaItems.swift
+++ Depo/Depo/Business/CoreData/LocalMediaItems.swift
@@ -28,7 +28,7 @@ extension CoreDataStack {
     
     func append(localMediaItems: [PHAsset], completion: @escaping VoidHandler) {
 //        let newBgcontext = backgroundContext//self.newChildBackgroundContext
-        save(items: localMediaItems, context: backgroundContext, completion: completion)
+        save(items: localMediaItems, context: newChildBackgroundContext, completion: completion)
     }
     
     func remove(localMediaItems: [PHAsset], completion: @escaping VoidHandler) {
