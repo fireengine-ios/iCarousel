8bbc59cb6d9d008a980f55641381475dad21304c
OlegRidevskij <oleg@MacBook-Pro-Oleg.local>
2018-04-25 09:24:36 +0300

    LR-5363

--- Depo/Depo/Presentation/UserStories/Grid/BaseDataSources/BaseDataSourceForCollectionView.swift
+++ Depo/Depo/Presentation/UserStories/Grid/BaseDataSources/BaseDataSourceForCollectionView.swift
@@ -1613,12 +1613,11 @@ UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, ItemOperationMan
         if isLocalOnly(){
             return
         }
-        if let unwrapedFilters = originalFilters  {
-            if isFavoritesOnly(filters: unwrapedFilters) || isAlbumDetail(filters: unwrapedFilters){
-                return
-            }
-        }
-        
+//        if let unwrapedFilters = originalFilters  {
+//            if isFavoritesOnly(filters: unwrapedFilters) || isAlbumDetail(filters: unwrapedFilters){
+//                return
+//            }
+//        }
         delegate?.needReloadData()
     }
     
