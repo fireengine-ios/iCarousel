b49b74d6d9a2aa25f2e8b994487d07fd8802c4f9
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2018-05-02 15:00:12 +0300

    fixed buttons with images on home screen

--- Depo/Depo/Presentation/Buttons/WhiteButtonWithRoundedCorner.swift
+++ Depo/Depo/Presentation/Buttons/WhiteButtonWithRoundedCorner.swift
@@ -39,6 +39,8 @@ class WhiteButtonWithRoundedCorner: InsetsButton {
         layer.cornerRadius = frame.height * 0.5
         
         let inset = frame.height * 0.3
-        insets = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
+        let isAddedImaged = image(for: .normal) != nil
+        let leftInset: CGFloat = isAddedImaged ? 0 : inset
+        insets = UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: inset)
     }
 }
