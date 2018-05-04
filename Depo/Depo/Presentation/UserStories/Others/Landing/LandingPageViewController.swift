8fed9c988aa05334ca1291516326388354677a54
OlegRidevskij <oleg@MacBook-Pro-Oleg.local>
2018-05-04 09:13:43 +0300

    LR-4915

---
+++ Depo/Depo/Presentation/UserStories/Others/Landing/LandingPageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LandingPageViewController.swift
+//  Depo
+//
+//  Created by Oleg on 03.05.2018.
+//  Copyright © 2018 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
