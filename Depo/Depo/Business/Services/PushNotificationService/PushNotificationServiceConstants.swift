6a3def9fd9d01cda8af185d39c1acfd2d9e54c9c
Andrei Novikov <anovikov.brest@gmail.com>
2018-05-11 15:33:59 +0300

    LR-5595 home page tag

--- Depo/Depo/Business/Services/PushNotificationService/PushNotificationServiceConstants.swift
+++ Depo/Depo/Business/Services/PushNotificationService/PushNotificationServiceConstants.swift
@@ -39,7 +39,7 @@ enum PushNotificationAction: String {
     case http = "http"
     case login = "login"
     case search = "search"
-    case freeUpSpace = "freeUpSpace"
-    case home = "home"
+    case freeUpSpace = "free_up_space" //TODO: check tag after implement on server
+    case home = "home_page"
 }
 
