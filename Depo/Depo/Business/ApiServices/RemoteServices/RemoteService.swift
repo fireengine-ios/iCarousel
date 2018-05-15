d9dbc466a6748735bba226ed645f237afcb86f85
Konstantin Studilin <konstantin.studilin@life.com.by>
2018-05-15 17:00:30 +0300

    removed GLOBAL global queue

--- Depo/Depo/Business/ApiServices/RemoteServices/RemoteService.swift
+++ Depo/Depo/Business/ApiServices/RemoteServices/RemoteService.swift
@@ -179,20 +179,21 @@ final class NextPageOperation: Operation {
     
     override func main() {
         requestTask = searchService.searchByField(param: requestParam, success: { [weak self] response  in
-            guard let `self` = self else {
-                return
-            }
-            
-            guard let resultResponse = (response as? SearchResponse)?.list else {
-                self.fail?()
+            DispatchQueue.toBackground { [weak self] in
+                guard let `self` = self else {
+                    return
+                }
+                
+                guard let resultResponse = (response as? SearchResponse)?.list else {
+                    self.fail?()
+                    self.semaphore.signal()
+                    return
+                }
+                
+                let list = resultResponse.flatMap { WrapData(remote: $0) }
+                self.success?(list)
                 self.semaphore.signal()
-                return
             }
-            
-            let list = resultResponse.flatMap { WrapData(remote: $0) }
-            self.success?(list)
-            self.semaphore.signal()
-            
         }, fail: { [weak self] errorResponce in
             
             /// temp error handling
