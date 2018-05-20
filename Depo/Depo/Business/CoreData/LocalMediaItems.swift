4ea8de69cc3103d2c317755f607df5ea27e1d7a3
Konstantin Studilin <konstantin.studilin@life.com.by>
2018-05-20 13:48:29 +0300

    toMain

--- Depo/Depo/Business/CoreData/LocalMediaItems.swift
+++ Depo/Depo/Business/CoreData/LocalMediaItems.swift
@@ -78,7 +78,7 @@ extension CoreDataStack {
             NotificationCenter.default.post(name: Notification.Name.allLocalMediaItemsHaveBeenLoaded, object: nil)
             return
         }
-        DispatchQueue.main.async {
+        DispatchQueue.toMain {
             JDStatusBarNotification.show(withStatus: "LOCAL FILES BEING POCCESSED", styleName: "JDStatusBarStyleWarning")
             JDStatusBarNotification.showActivityIndicator(true, indicatorStyle: .gray)
         }
@@ -86,7 +86,7 @@ extension CoreDataStack {
         print("All local files started  \((start)) seconds")
         nonCloudAlreadySavedAssets.dropAll()
         save(items: notSaved, context: backgroundContext) { [weak self] in
-            DispatchQueue.main.async {
+            DispatchQueue.toMain {
                 JDStatusBarNotification.show(withStatus: "ALL DONE", styleName: "JDStatusBarStyleSuccess")
                 JDStatusBarNotification.dismiss(animated: true)
             }
