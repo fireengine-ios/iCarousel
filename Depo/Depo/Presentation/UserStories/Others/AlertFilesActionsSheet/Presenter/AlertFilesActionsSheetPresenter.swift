dc9e30e555feba2c6a1e69b8936dc928d834f877
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2018-05-21 15:12:21 +0300

    fixed LR-5671 iOS: Select button is not compatible with page design (Tablet)

--- Depo/Depo/Presentation/UserStories/Others/AlertFilesActionsSheet/Presenter/AlertFilesActionsSheetPresenter.swift
+++ Depo/Depo/Presentation/UserStories/Others/AlertFilesActionsSheet/Presenter/AlertFilesActionsSheetPresenter.swift
@@ -10,7 +10,7 @@ class AlertFilesActionsSheetPresenter: MoreFilesActionsPresenter, AlertFilesActi
     
     private let semaphore = DispatchSemaphore(value: 0)
     
-    let rightButtonBox = CGRect(x: Device.winSize.width - 50, y: 64, width: 10, height: 10)
+    let rightButtonBox = CGRect(x: Device.winSize.width - 50, y: -30, width: 0, height: 0)
     // MARK: Module Input
     
     func showSelectionsAlertSheet() {
@@ -463,6 +463,7 @@ class AlertFilesActionsSheetPresenter: MoreFilesActionsPresenter, AlertFilesActi
         } else if let _ = sender as? UIBarButtonItem {
             //FIXME: use actionSheetVC.popoverPresentationController?.barButtonItem instead
             actionSheetVC.popoverPresentationController?.sourceRect = rightButtonBox
+            actionSheetVC.popoverPresentationController?.permittedArrowDirections = []
         }
         vc.present(actionSheetVC, animated: true, completion: {})
     }
