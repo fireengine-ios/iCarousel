47bf60425f6c98345ffe4d82b66df2a5f133df49
AleksandrP <Aleksandr.Pestriakov@life.com.by>
2018-08-10 12:06:13 +0300

    no message

---
+++ Depo/Depo/Business/CoreData/RemoteMediaItems.swift
@@ -0,0 +1,31 @@
+//
+//  RemoteMediaItems.swift
+//  Depo
+//
+//  Created by Aleksandr on 8/10/18.
+//  Copyright © 2018 LifeTech. All rights reserved.
+//
+
+class CacheManager {///adding files TO DB // managing cache
+    
+    func startAppendingAllRemotes() {
+        
+    }
+    
+    func startAppendingAllLocals() {
+        
+    }
+    
+    
+    
+}
+
+//class <#name#>: <#super class#> {
+//    <#code#>
+//}
+
+extension CoreDataStack {
+
+//    func
+    
+}
