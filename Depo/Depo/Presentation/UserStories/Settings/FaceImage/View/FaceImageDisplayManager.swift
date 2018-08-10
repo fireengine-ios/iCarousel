732df52a4b26fcb463500bafc080321c5388e25b
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2018-08-10 10:29:43 +0300

    started FE-40 iOS: Facebook Tag Transfer

---
+++ Depo/Depo/Presentation/UserStories/Settings/FaceImage/View/FaceImageDisplayManager.swift
@@ -0,0 +1,20 @@
+//
+//  FaceImageDisplayManager.swift
+//  Depo_LifeTech
+//
+//  Created by Bondar Yaroslav on 8/10/18.
+//  Copyright © 2018 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+final class FaceImageDisplayManager: NSObject {
+    
+    @IBOutlet weak var facebookView: UIView!
+    @IBOutlet weak var labelsStackView: UIStackView!
+    @IBOutlet weak var facebookImportButton: UIButton!
+    @IBOutlet weak var firstFacebookLabel: UILabel!
+    @IBOutlet weak var secondFacebookLabel: UILabel!
+    
+    
+}
--- Depo/Depo/Presentation/UserStories/Settings/FaceImage/View/FaceImageViewController.swift
+++ Depo/Depo/Presentation/UserStories/Settings/FaceImage/View/FaceImageViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 final class FaceImageViewController: ViewController {
     
     var output: FaceImageViewOutput!
