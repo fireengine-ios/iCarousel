9b38c36c91ce5482c27990d28a3287712d53b3e9
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2018-08-16 17:24:43 +0300

    created fetchedResultsController

--- Depo/Depo/Presentation/SubViews/CustomTabBars/PhotoVideoController.swift
+++ Depo/Depo/Presentation/SubViews/CustomTabBars/PhotoVideoController.swift
@@ -12,7 +12,8 @@ final class PhotoVideoController: UIViewController, NibInit {
 
     @IBOutlet private weak var collectionView: UICollectionView! {
         didSet {
-//            collectionView.dataSource =
+            collectionView.dataSource = self
+            collectionView.delegate = self
         }
     }
     
@@ -33,6 +34,16 @@ final class PhotoVideoController: UIViewController, NibInit {
         setupPullToRefresh()
 //        collectionView.alwaysBounceVertical = true
 //        collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 25, right: 0)
+        
+        
+        
+        fetchedResultsController.delegate = self
+        /// default main queue fetch
+        try? fetchedResultsController.performFetch()
+        collectionView.reloadData()
+        
+//        CollectionViewCellsIdsConstant.cellForImage
+        collectionView.register(nibCell: CollectionViewCellForPhoto.self)
     }
     
     private func setupEdittingBar() {
@@ -57,4 +68,141 @@ final class PhotoVideoController: UIViewController, NibInit {
     @objc private func onCancelSelectionButton() {
         
     }
+    
+    private lazy var sectionChanges = [() -> Void]()
+    private lazy var objectChanges = [() -> Void]()
+    
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<MediaItem> = {
+        let fetchRequest: NSFetchRequest = MediaItem.fetchRequest()
+        let sortDescriptor2 = NSSortDescriptor(key: #keyPath(MediaItem.creationDateValue), ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor2]
+        
+        // TODO: device isIpad
+        if UI_USER_INTERFACE_IDIOM() == .pad {
+            fetchRequest.fetchBatchSize = 50
+        } else {
+            fetchRequest.fetchBatchSize = 20
+        }
+        
+        //fetchRequest.relationshipKeyPathsForPrefetching = [#keyPath(PostDB.id)]
+        let context = CoreDataStack.default.mainContext
+        return NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+    }()
+}
+
+extension PhotoVideoController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+
+        let cell_ = collectionView.dequeue(cell: CollectionViewCellForPhoto.self, for: indexPath)
+        let object = fetchedResultsController.object(at: indexPath)
+        let wraped = WrapData(mediaItem: object)
+        
+        switch wraped.patchToPreview {
+        case .localMediaContent(let local):
+            cell_.setAssetId(local.asset.localIdentifier)
+            FilesDataSource().getAssetThumbnail(asset: local.asset, indexPath: indexPath, completion: { (image, path) in
+                DispatchQueue.main.async {
+                    if cell_.getAssetId() == local.asset.localIdentifier, let image = image {
+                        cell_.setImage(image: image, animated:  false)
+                    } else {
+                        cell_.setPlaceholderImage(fileType: wraped.fileType)
+                    }
+                }
+            })
+
+        default:
+            break
+        }
+        
+        return cell_
+    }
+}
+
+extension PhotoVideoController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+/// https://github.com/jessesquires/JSQDataSourcesKit/blob/develop/Source/FetchedResultsDelegate.swift
+/// https://gist.github.com/nor0x/c48463e429ba7b053fff6e277c72f8ec
+extension PhotoVideoController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        sectionChanges.removeAll()
+        objectChanges.removeAll()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+        let section = IndexSet(integer: sectionIndex)
+        
+        sectionChanges.append { [unowned self] in
+            switch type {
+            case .insert:
+                self.collectionView.insertSections(section)
+            case .delete:
+                self.collectionView.deleteSections(section)
+            default:
+                break
+            }
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            if let indexPath = newIndexPath {
+                self.objectChanges.append { [unowned self] in
+                    self.collectionView.insertItems(at: [indexPath])
+                }
+            }
+        case .delete:
+            if let indexPath = indexPath {
+                self.objectChanges.append { [unowned self] in
+                    self.collectionView.deleteItems(at: [indexPath])
+                }
+            }
+        case .update:
+            if let indexPath = indexPath {
+                self.objectChanges.append { [unowned self] in
+                    self.collectionView.reloadItems(at: [indexPath])
+                }
+            }
+        case .move:
+            if let indexPath = indexPath, let newIndexPath = newIndexPath {
+                self.objectChanges.append { [unowned self] in
+                    self.collectionView.moveItem(at: indexPath, to: newIndexPath)
+                }
+            }
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        collectionView.performBatchUpdates({ [weak self] in  
+            self?.objectChanges.forEach { $0() }
+//            self?.sectionChanges.forEach { $0() }
+        }, completion: { [weak self] _ in
+            
+            self?.collectionView.performBatchUpdates({
+                self?.sectionChanges.forEach { $0() }
+            }, completion: { _ in 
+                self?.reloadSupplementaryViewsIfNeeded()
+            })
+        })
+    }
+    
+    private func reloadSupplementaryViewsIfNeeded() {
+        if !sectionChanges.isEmpty {
+            collectionView.reloadData()
+        }
+    }
+
 }
