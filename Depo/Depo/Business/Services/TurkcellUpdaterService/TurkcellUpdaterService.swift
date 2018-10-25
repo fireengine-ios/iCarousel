aeea8ffaa3283121c6692bb74d27171aaa534561
Konstantin Studilin <konstantin.studilin@life.com.by>
2018-10-25 17:10:12 +0300

    stop the app flow

--- Depo/Depo/Business/Services/TurkcellUpdaterService/TurkcellUpdaterService.swift
+++ Depo/Depo/Business/Services/TurkcellUpdaterService/TurkcellUpdaterService.swift
@@ -18,13 +18,16 @@ final class TurkcellUpdaterService {
             case .none:
                 ///never
                 print("UpdaterController: none")
+                
             case .updateFound:
                 ///if parentController == nil
                 print("UpdaterController: updateFound")
                 
             case .updateChosen:
                 /// OK or Install
+                /// Stop the app flow
                 print("UpdaterController: updateChosen")
+                return
                 
             case .updateCheckCompleted:
                 ///Cancelled or not found or failed
