550eae75bf070c65500d760fa692594bec8ad669
Raman Harhun <roman.gorgun@orangesoft.by>
2018-12-18 11:13:29 +0300

    minor fix

--- Depo/Depo/Presentation/UserStories/Home/HomePage/Presenter/HomePagePresenter.swift
+++ Depo/Depo/Presentation/UserStories/Home/HomePage/Presenter/HomePagePresenter.swift
@@ -119,6 +119,9 @@ class HomePagePresenter: HomePageModuleInput, HomePageViewOutput, HomePageIntera
         
         if isReloadAll {
             CardsManager.default.startOperatonsForCardsResponces(cardsResponces: cards)
+        } else {
+            //to hide spinner when refresh only premium card
+            view.stopRefresh()
         }
     }
 }
