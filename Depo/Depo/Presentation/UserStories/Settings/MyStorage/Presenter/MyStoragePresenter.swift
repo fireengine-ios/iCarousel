dc50fa15cb03470057ea0102ce1da7682068a59c
Raman Harhun <roman.gorgun@orangesoft.by>
2018-12-28 12:37:53 +0300

    mini minor fix

--- Depo/Depo/Presentation/UserStories/Settings/MyStorage/Presenter/MyStoragePresenter.swift
+++ Depo/Depo/Presentation/UserStories/Settings/MyStorage/Presenter/MyStoragePresenter.swift
@@ -21,10 +21,7 @@ final class MyStoragePresenter {
                 router.showSubTurkcellOpenAlert(with: TextConstants.offersActivateUkranian)
             case .cyprus:
                 router.showSubTurkcellOpenAlert(with: TextConstants.offersActivateCyprus)
-            case .moldovian: break
-            case .turkcell: break
-            case .life: break
-            case .all: break
+            case .moldovian, .turkcell, .life, .all: break
             }
         }
     }
