3d8034ecde3e7d57390e8a7254d042066ae85124
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2019-01-10 15:53:01 +0300

    started InstapickService

---
+++ Depo/Depo/Business/ApiServices/InstapickService.swift
@@ -0,0 +1,43 @@
+//
+//  InstapickService.swift
+//  Depo_LifeTech
+//
+//  Created by Bondar Yaroslav on 1/10/19.
+//  Copyright © 2019 LifeTech. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+
+final class InstapickService {
+    
+    let sessionManager: SessionManager
+    
+    init(sessionManager: SessionManager = SessionManager.customDefault) {
+        self.sessionManager = sessionManager
+    }
+    
+    func getThumbnails(handler: @escaping (ResponseResult<[URL]>) -> Void) {
+        sessionManager
+            .request(RouteRequests.Instapick.thumbnails)
+            .customValidate()
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    guard let jsonArray = JSON(data: data).array else {
+                        let error = CustomErrors.serverError("\(RouteRequests.Instapick.thumbnails) not array in response")
+                        handler(.failed(error))
+                        return
+                    }
+                    
+                    let results = jsonArray
+                        .flatMap { $0.string }
+                        .flatMap { URL(string: $0) }
+                    
+                    handler(.success(results))
+                case .failure(let error):
+                    handler(.failed(error))
+                }
+        }
+    }
+}
