9c327b395074b8fb93a592e686cc0d8b2ecba6c4
Raman Harhun <roman.gorgun@orangesoft.by>
2019-01-31 11:05:04 +0300

    refactoring

---
+++ Depo/Depo/Presentation/SubViews/InstaPickPhotoView/InstaPickBigPhotoView.swift
@@ -0,0 +1,41 @@
+//
+//  InstaPickBigPhotoView.swift
+//  Depo
+//
+//  Created by Raman Harhun on 1/30/19.
+//  Copyright © 2019 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+final class InstaPickBigPhotoView: InstaPickPhotoView {
+
+    override func setupLayout(isIPad: Bool) {
+        super.setupLayout(isIPad: isIPad)
+        
+        //relation constraints
+        rateViewCenterYConstraint.constraintWithMultiplier(1.5).isActive = true
+        pickedViewCenterXConstraint.constraintWithMultiplier(0.5).isActive = true
+
+        //size constraints
+        rateView.heightAnchor.constraint(equalToConstant: isIPad ? 40 : 30).isActive = true
+        pickedView.widthAnchor.constraint(equalToConstant: isIPad ? 95 : 70).isActive = true
+        pickedView.heightAnchor.constraint(equalToConstant: isIPad ? 40 : 30).isActive = true
+        
+        imageViewHeightConstraint.constraintWithMultiplier(0.97).isActive = true
+    }
+    
+    override func setupFonts(isIPad: Bool) {
+        super.setupFonts(isIPad: isIPad)
+
+        rateLabel.font = UIFont.TurkcellSaturaBolFont(size: isIPad ? 20 : 14)
+    }
+    
+    override func getPhotoUrl() -> URL? {
+        return model?.getLargeImageURL()
+    }
+    
+    override func isNeedHidePickedView(hasSmallPhotos: Bool) -> Bool {
+        return !hasSmallPhotos
+    }
+}
