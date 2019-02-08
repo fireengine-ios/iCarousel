e47235c38c1ced3b63fdaf60a0d77a49063d0ab3
Daria.Kuleshova <daria.kuleshova@life.com.by>
2019-02-08 16:50:41 +0300

    Use of UILabel extension for minScaleFactor

--- Depo/Depo/Presentation/SubViews/PackagePremiumView/PackagePremiumView.swift
+++ Depo/Depo/Presentation/SubViews/PackagePremiumView/PackagePremiumView.swift
@@ -27,8 +27,7 @@ final class PackagePremiumView: UIView, NibInit {
     private func setup() {
         titleLabel.font = UIFont.TurkcellSaturaBolFont(size: 20)
         titleLabel.textColor = ColorConstants.darkText
-        titleLabel.adjustsFontSizeToFitWidth = true
-        titleLabel.minimumScaleFactor = 0.1
+        titleLabel.adjustsFontSizeToFitWidth()
 
         messageLabel.font = UIFont.TurkcellSaturaRegFont(size: 16)
         messageLabel.textColor = ColorConstants.textGrayColor
