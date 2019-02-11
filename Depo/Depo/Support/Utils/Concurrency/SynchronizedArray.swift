75917d5491c9d464bd69330e9c5f7e4f3dbef686
TURKCELL\EXT02D13490 <andrei.novikau@consultant.turkcell.com.tr>
2019-02-11 14:40:58 +0300

    FE-710 Crash - SynchronizedArray.remove(at:completion:)

--- Depo/Depo/Support/Utils/Concurrency/SynchronizedArray.swift
+++ Depo/Depo/Support/Utils/Concurrency/SynchronizedArray.swift
@@ -176,12 +176,15 @@ public extension SynchronizedArray {
     /// - Parameters:
     ///   - index: The position of the element to remove.
     ///   - completion: The handler with the removed element.
-    func remove(at index: Int, completion: ((Element) -> Void)? = nil) {
+    func safeRemove(at index: Int, completion: ((Element) -> Void)? = nil) {
         queue.async(flags: .barrier) {
-            let element = self.array.remove(at: index)
-            
-            DispatchQueue.main.async {
-                completion?(element)
+            let isIndexValid = self.array.indices.contains(index)
+            if isIndexValid {
+                let element = self.array.remove(at: index)
+                
+                DispatchQueue.main.async {
+                    completion?(element)
+                }
             }
         }
     }
