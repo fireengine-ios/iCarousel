6e1101da74eed33a791f4406b7eba7c4494186c5
TURKCELL\EXT02D13490 <andrei.novikau@consultant.turkcell.com.tr>
2019-03-01 16:36:26 +0300

    predicate format fix

--- Depo/Depo/Business/CoreData/MediaItemOperationsService.swift
+++ Depo/Depo/Business/CoreData/MediaItemOperationsService.swift
@@ -208,7 +208,7 @@ final class MediaItemOperationsService {
             return
         }
 
-        let predicateForRemoteFiles = NSPredicate(format: "trimmedLocalFileID == %@ AND md5Value == %@ AND isLocalItemValue == false", uuid, md5)
+        let predicateForRemoteFiles = NSPredicate(format: "(trimmedLocalFileID == %@ OR md5Value == %@) AND isLocalItemValue == false)", uuid, md5)
         
         executeRequest(predicate: predicateForRemoteFiles, context: context) { alreadySavedRemoteItems in
             alreadySavedRemoteItems.forEach({ savedItem in
