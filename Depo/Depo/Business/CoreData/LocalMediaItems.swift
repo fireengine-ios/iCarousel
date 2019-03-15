0ce98e61d04bbee8dea0fe0bdbac30fe4ece6f10
AleksandrP <Aleksandr.Pestriakov@life.com.by>
2019-03-15 17:19:11 +0300

    Should help fix duplication problem. Maybe we can implement faster solution

--- Depo/Depo/Business/CoreData/LocalMediaItems.swift
+++ Depo/Depo/Business/CoreData/LocalMediaItems.swift
@@ -52,8 +52,17 @@ extension CoreDataStack {
                 }
             }
             LocalMediaStorage.default.assetsCache.append(list: newAssets)
+            let context = self.newChildBackgroundContext
             
-            self.save(items: newAssets, context: self.newChildBackgroundContext, completion: completion)
+            let alreadySavedMediaItems = self.executeRequest(predicate: NSPredicate(format: " (\(#keyPath(MediaItem.isLocalItemValue)) == true) AND (\(#keyPath(MediaItem.localFileID)) IN %@)", newAssets.map{$0.localIdentifier}), context: context)
+            
+            debugLog("new assets to add without trimming \(newAssets.count)")
+            alreadySavedMediaItems.forEach { alreadySavedItem in
+                newAssets.removeAll(where: {  $0.localIdentifier == alreadySavedItem.localFileID })
+            }
+            debugLog("new assets to add after trimming \(newAssets.count)")
+                
+            self.save(items: newAssets, context: context, completion: completion)
         }
     }
     
