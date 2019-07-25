1b0d8df3ea4406908fce0c5d961e4d4aa1915d69
TURKCELL\EXT02D13490 <andrei.novikau@consultant.turkcell.com.tr>
2019-07-25 10:29:41 +0300

    FE-1359 Restore Purchase Button Should Appear For Only Non-Turkcell Accounts

--- Depo/Depo/Presentation/UserStories/Settings/MyStorage/Presenter/MyStoragePresenter.swift
+++ Depo/Depo/Presentation/UserStories/Settings/MyStorage/Presenter/MyStoragePresenter.swift
@@ -117,6 +117,9 @@ extension MyStoragePresenter: MyStorageInteractorOutput {
         if let accountTypeString = accountInfo.accountType {
             accountType = interactor.getAccountType(with: accountTypeString, offers: allOffers)
         }
+        if accountType == .all {
+            view?.showRestoreButton()
+        }
         interactor.getAllOffers(with: accountType)
     }
     
