be6c751118eb652cd6221f3925e11f5e22b1ccc3
Konstantin <>
2019-08-01 16:38:38 +0300

    remove creds

--- Depo/Depo/Presentation/UserStories/Start/Login/View/LoginViewController.swift
+++ Depo/Depo/Presentation/UserStories/Start/Login/View/LoginViewController.swift
@@ -141,9 +141,9 @@ final class LoginViewController: ViewController {
         output.viewIsReady()
         
         #if DEBUG
-        loginEnterView.textField.text = "+375447394882"//"qwerty@my.com"// "test3@test.test"//"test2@test.test"//"testasdasdMail@notRealMail.yep"
+        loginEnterView.textField.text = "qwerty@my.com"// "test3@test.test"//"test2@test.test"//"testasdasdMail@notRealMail.yep"
 
-        passwordEnterView.textField.text = "qazwsx"//"qwerty"// "zxcvbn"//".FsddQ646"
+        passwordEnterView.textField.text = "qwerty"// "zxcvbn"//".FsddQ646"
         #endif
     }
     
