cf420cb414a540dcda08fd2c35cdde7e76c431af
Konstantin <>
2019-08-06 11:27:56 +0300

    show loader under the top controller

--- Depo/Depo/Presentation/UserStories/Others/MoreFilesActions/Interactor/MoreFilesActionsInteractor.swift
+++ Depo/Depo/Presentation/UserStories/Others/MoreFilesActions/Interactor/MoreFilesActionsInteractor.swift
@@ -478,9 +478,9 @@ class MoreFilesActionsInteractor: NSObject, MoreFilesActionsInteractorInput {
                                      fail: failAction(elementType: .download))
             }
         } else if let albums = item as? [AlbumItem] {
-            output?.startAsyncOperation()
+            router.navigationController?.topViewController?.showSpinnerIncludeNavigationBar()
             photosAlbumService.loadItemsBy(albums: albums, success: {[weak self] itemsByAlbums in
-                self?.output?.asyncOperationSuccess()
+                self?.router.navigationController?.topViewController?.hideSpinnerIncludeNavigationBar()
                 self?.fileService.download(itemsByAlbums: itemsByAlbums,
                                            success: self?.succesAction(elementType: .download),
                                            fail: self?.failAction(elementType: .download))
