3f229c6cb7f5a7f7878b0b618cb7340ef41edf5b
Raman Harhun <roman.gorgun@orangesoft.by>
2019-08-30 15:04:39 +0300

    change request logic on pop up presenting

--- Depo/Depo/Presentation/UserStories/Others/VerifyEmailPopUp/VerifyEmailPopUp.swift
+++ Depo/Depo/Presentation/UserStories/Others/VerifyEmailPopUp/VerifyEmailPopUp.swift
@@ -112,9 +112,7 @@ final class VerifyEmailPopUp: UIViewController {
     private var isRemoveLetter: Bool = false
     private var currentSecurityCode = ""
     private var inputTextLimit = 6
-    
-    var isNeedSendCode: Bool = true
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -138,7 +136,7 @@ final class VerifyEmailPopUp: UIViewController {
         })
         
         ///don't send code if just registered(code already sent)
-        if SingletonStorage.shared.isJustRegistered == false && isNeedSendCode {
+        if SingletonStorage.shared.isJustRegistered == true {
             resendCode()
         }
     }
