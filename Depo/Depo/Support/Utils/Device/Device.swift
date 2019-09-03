1a204a45fac50b06cafc33efbe48c609d18833a7
Raman Harhun <roman.gorgun@orangesoft.by>
2019-09-03 16:52:16 +0300

    fix tabBar appearance

--- Depo/Depo/Support/Utils/Device/Device.swift
+++ Depo/Depo/Support/Utils/Device/Device.swift
@@ -52,6 +52,16 @@ class Device {
         return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last!
     }
     
+    static var isIOS13: Bool {
+        var isIOS13 = false
+        
+        if #available(iOS 13.0, *) {
+            isIOS13 = true
+        }
+        
+        return isIOS13
+    }
+    
     /// https://stackoverflow.com/questions/46192280/detect-if-the-device-is-iphone-x
     static var isIphoneX: Bool {
         return (UIDevice.current.userInterfaceIdiom == .phone) && (UIScreen.main.bounds.height >= 812)
