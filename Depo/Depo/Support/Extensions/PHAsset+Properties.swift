ec672d4cdfe70097951fa55e0775456961bc1ce7
Konstantin <>
2019-09-05 15:49:52 +0300

    use old method to get filename

--- Depo/Depo/Support/Extensions/PHAsset+Properties.swift
+++ Depo/Depo/Support/Extensions/PHAsset+Properties.swift
@@ -18,10 +18,15 @@ extension PHAsset {
     }
     
     var originalFilename: String? {
-        if #available(iOS 9.0, *) {
-            return resource?.originalFilename
-        } else {
+        if #available(iOS 13.0, *) {
+            //TODO: check again on GM or release ios 13 version
+            /* ios 13.1 beta returns:
+             - Adjustments.plist as originalFilename for the photos with enabled filter and slo-mo
+             - *.MOV for the live photos with Most Compatible format
+            */
             return value(forKey: "filename") as? String
+        } else {
+            return resource?.originalFilename
         }
     }
     
