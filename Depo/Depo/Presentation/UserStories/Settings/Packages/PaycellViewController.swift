bab47502c2ef15d5d2e69c837696c97440cd386a
Konstantin <>
2019-09-19 09:07:50 +0300

    added new controller

---
+++ Depo/Depo/Presentation/UserStories/Settings/Packages/PaycellViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PaycellViewController.swift
+//  Depo
+//
+//  Created by Konstantin Studilin on 18/09/2019.
+//  Copyright © 2019 LifeTech. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+
+final class PaycellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
