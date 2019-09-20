12e71c7c3e34635ecf44750adf0bc4ff238f77d8
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2019-09-20 10:24:49 +0300

    added comment

--- Depo/Depo/Presentation/UserStories/Settings/Packages/View/PackagesViewController.swift
+++ Depo/Depo/Presentation/UserStories/Settings/Packages/View/PackagesViewController.swift
@@ -155,6 +155,8 @@ extension PackagesViewController: PackagesViewInput {
                 
             })
         }
+        
+        /// we need ti show popup over popup with purchase options
         RouterVC().defaultTopController?.present(vc, animated: false, completion: nil)
     }
 
