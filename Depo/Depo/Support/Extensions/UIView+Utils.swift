b259a386de14f6c8f44528771fab20655b953a50
Darya Kuliashova <kuliashova.darya@gmail.com>
2019-10-15 18:04:17 +0300

    Sort by is displayed correctly for ios 13 and less

--- Depo/Depo/Support/Extensions/UIView+Utils.swift
+++ Depo/Depo/Support/Extensions/UIView+Utils.swift
@@ -9,8 +9,32 @@
 import UIKit
 
 extension UIView {
-    func setSubviewsHidden(_ isHidden: Bool) {
-        subviews.forEach({ $0.isHidden = isHidden })
+    var safeTopAnchor: NSLayoutYAxisAnchor {
+      if #available(iOS 13.0, *) {
+        return self.safeAreaLayoutGuide.topAnchor
+      }
+      return self.topAnchor
+    }
+
+    var safeLeadingAnchor: NSLayoutXAxisAnchor {
+      if #available(iOS 13.0, *){
+        return self.safeAreaLayoutGuide.leadingAnchor
+      }
+      return self.leadingAnchor
+    }
+
+    var safeTrailingAnchor: NSLayoutXAxisAnchor {
+      if #available(iOS 13.0, *){
+        return self.safeAreaLayoutGuide.trailingAnchor
+      }
+      return self.trailingAnchor
+    }
+
+    var safeBottomAnchor: NSLayoutYAxisAnchor {
+      if #available(iOS 13.0, *) {
+        return self.safeAreaLayoutGuide.bottomAnchor
+      }
+      return self.bottomAnchor
     }
 
     var frameOnWindow: CGRect {
@@ -18,6 +42,10 @@ extension UIView {
             return frame
         }
         return superview.convert(frame, to: window)
+    }
+    
+    func setSubviewsHidden(_ isHidden: Bool) {
+        subviews.forEach({ $0.isHidden = isHidden })
     }
     
     func pinToSuperviewEdges(offset: CGFloat = 0.0) {
@@ -64,5 +92,4 @@ extension UIView {
         subviews.forEach { checkViewForType($0) }
         return viewWeAreLookingFor
     }
-    
 }
