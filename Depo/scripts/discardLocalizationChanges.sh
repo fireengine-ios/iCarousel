56a193627ac28b08f6ce294db4e67aacd04ce148
AleksandrP <Aleksandr.Pestriakov@life.com.by>
2019-11-20 15:46:26 +0300

    whole folder discard

--- Depo/scripts/discardLocalizationChanges.sh
+++ Depo/scripts/discardLocalizationChanges.sh
@@ -6,15 +6,16 @@
 #  Created by Alex on 11/20/19.
 #  Copyright © 2019 LifeTech. All rights reserved.
 
-declare -a arr=("$SRCROOT/Depo/App/Resources/en.lproj/OurLocalizable.strings" "$SRCROOT/Depo/App/Resources/ar.lproj/OurLocalizable.strings"
-    "$SRCROOT/Depo/App/Resources/de.lproj/OurLocalizable.strings"
-    "$SRCROOT/Depo/App/Resources/es.lproj/OurLocalizable.strings"
-    "$SRCROOT/Depo/App/Resources/fr.lproj/OurLocalizable.strings"
-    "$SRCROOT/Depo/App/Resources/ro.lproj/OurLocalizable.strings"
-    "$SRCROOT/Depo/App/Resources/ru.lproj/OurLocalizable.strings"
-    "$SRCROOT/Depo/App/Resources/sq.lproj/OurLocalizable.strings"
-    "$SRCROOT/Depo/App/Resources/tr.lproj/OurLocalizable.strings"
-    "$SRCROOT/Depo/App/Resources/uk.lproj/OurLocalizable.strings"
+declare -a arr=("$SRCROOT/Depo/App/Resources/en.lproj"
+    "$SRCROOT/Depo/App/Resources/ar.lproj"
+    "$SRCROOT/Depo/App/Resources/de.lproj"
+    "$SRCROOT/Depo/App/Resources/es.lproj"
+    "$SRCROOT/Depo/App/Resources/fr.lproj"
+    "$SRCROOT/Depo/App/Resources/ro.lproj"
+    "$SRCROOT/Depo/App/Resources/ru.lproj"
+    "$SRCROOT/Depo/App/Resources/sq.lproj"
+    "$SRCROOT/Depo/App/Resources/tr.lproj"
+    "$SRCROOT/Depo/App/Resources/uk.lproj"
     )
 
 for i in "${arr[@]}"
