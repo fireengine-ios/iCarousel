372ac910a0d4bd8c253bac2377f8063d5df63bb3
Konstantin <>
2019-12-16 15:02:11 +0300

    temp success popup

---
+++ Depo/Depo/Presentation/UserStories/Others/SuccessfulHidePopUp/SuccessfullHidePopUp.swift
@@ -0,0 +1,18 @@
+//
+//  SuccessfullHidePopUp.swift
+//  Depo
+//
+//  Created by Konstantin Studilin on 16/12/2019.
+//  Copyright © 2019 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+class SuccessfullHidePopUp: BasePopUpController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
