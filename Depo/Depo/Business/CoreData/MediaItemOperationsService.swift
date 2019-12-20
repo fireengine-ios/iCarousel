76b4efad38f066535f05ba4e5c04093c81fe2c6a
Konstantin <>
2019-12-20 09:55:27 +0300

    minor fix

--- Depo/Depo/Business/CoreData/MediaItemOperationsService.swift
+++ Depo/Depo/Business/CoreData/MediaItemOperationsService.swift
@@ -969,7 +969,7 @@ final class MediaItemOperationsService {
     //MARK: - Hide
     func hide(_ items: [WrapData], completion: @escaping VoidHandler) {
         coreDataStack.performBackgroundTask { [weak self] context in
-            guard let `self` = self else {
+            guard let self = self else {
                 return
             }
             
