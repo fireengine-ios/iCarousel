54ed33bab9fdf466ded841f71e2f0047b0c32061
ÜNAL ÖZTÜRK <unal.ozturk@consultant.turkcell.com.tr>
2019-12-24 11:18:37 +0300

    deleteSelectedItem() reversed and hideView() unwrapping type changed

--- Depo/Depo/Presentation/UserStories/Others/PhotoVideoDetail/Interactor/PhotoVideoDetailInteractor.swift
+++ Depo/Depo/Presentation/UserStories/Others/PhotoVideoDetail/Interactor/PhotoVideoDetailInteractor.swift
@@ -163,15 +163,8 @@ class PhotoVideoDetailInteractor: NSObject, PhotoVideoDetailInteractorInput {
         
         if array.isEmpty {
             /// added asyncAfter 1 sec to wait PopUpController about success deleting
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { 
                 self.output.goBack()
-                
-                ///it is also to dismiss popup
-                if type == .hide {
-                    self.output.goBack()
-                }
-                
             }
         } else {
             output.updateItems(objects: array, selectedIndex: selectedIndex, isRightSwipe: isRightSwipe)
