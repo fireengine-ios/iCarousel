ec5127915ac7fe67ee5ea37b97bb2a87564de592
AleksandrP <Aleksandr.Pestriakov@life.com.by>
2020-01-03 16:59:04 +0300

    netmera event as part of enum

--- Depo/Depo/Business/Services/AnalyticsService/Netmera/EventModels/NetmeraLoginEvent.swift
+++ Depo/Depo/Business/Services/AnalyticsService/Netmera/EventModels/NetmeraLoginEvent.swift
@@ -7,21 +7,27 @@
 //
 
 import Netmera
+extension NetmeraEvents {
+    final class NetmeraLoginEvent: NetmeraEvent {
+        
+        private let kLoginKey = "rvw"
+        
+        @objc var status: String = ""
+        @objc var loginType: String = ""
 
-class Login: NetmeraEvent {
-    
-    let kLoginKey = "rvw"
-    
-    @objc var status: String = ""
-    @objc var loginType: String = ""
-
-    override class func keyPathPropertySelectorMapping() -> [AnyHashable: Any] {
-        return[
-            "ea" : #keyPath(status),//NSStringFromSelector(#selector(getter: self.status)),
-            "eb" : #keyPath(loginType),//NSStringFromSelector(#selector(getter: self.loginType)),
-        ]
-    }
-    override var eventKey : String {
-        return kLoginKey
+//        init(status: String, loginType: String) {
+//            super.init()
+//            
+//        }
+        
+        override class func keyPathPropertySelectorMapping() -> [AnyHashable: Any] {
+            return[
+                "ea" : #keyPath(status),//NSStringFromSelector(#selector(getter: self.status)),
+                "eb" : #keyPath(loginType),//NSStringFromSelector(#selector(getter: self.loginType)),
+            ]
+        }
+        override var eventKey : String {
+            return kLoginKey
+        }
     }
 }
