9a6c807fc95bb57e093a442edce7f40b6dfa7528
AleksandrP <Aleksandr.Pestriakov@life.com.by>
2020-01-10 17:20:49 +0300

    netmera event values

--- Depo/Depo/Business/Services/AnalyticsService/Netmera/NetmeraService.swift
+++ Depo/Depo/Business/Services/AnalyticsService/Netmera/NetmeraService.swift
@@ -21,6 +21,21 @@ final class NetmeraService {
         let user = NetmeraUser()
         user.userId = SingletonStorage.shared.accountInfo?.gapId ?? ""
         
+//        NetmeraUser *user = [[NetmeraUser alloc] init];
+//        }}{{  user.autosync = @"On";
+//        autosyncPhotos: Never/Wifi/Wifi_LTE
+//        autosyncVideos: Never/Wifi/Wifi_LTE
+//        deviceStorage:{used_percentage_value} : It shows how many storage used on the device.
+//        lifeboxStorage:{used_percentage_value} : Settings/Account Detail/ My Storage
+//        faceImageGrouping: On/Off
+//        photopickLeftAnalysis:{count_of_left_analysis} : If user is premium, the value should be 'Free'.
+//        accountType: Standard/Standard+/Premium : Settings/Account Detail/ Account Type
+//        twoFactorAuthentication: On/Off
+//        emailVerification: Verified/NotVerified
+//
+
+        
+        
         Netmera.update(user)
     }
     
