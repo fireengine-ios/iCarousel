759c887c84811c9e65d81186fe03b570feca31f4
ÜNAL ÖZTÜRK <unal.ozturk@consultant.turkcell.com.tr>
2020-01-31 14:28:04 +0300

    Changed attempts numbers for Billo.

--- Depo/Depo/App/Constants/NumericConstants.swift
+++ Depo/Depo/App/Constants/NumericConstants.swift
@@ -16,8 +16,15 @@ struct NumericConstants {
     static let verificationCharacterLimit = 6
     static let verificationTimerLimit = 120//in seconds
     static let maxVerificationAttempts = 3
+    
+    #if LIFEBOX
     static let showFAQViewAttempts = 3
     static let showSupportViewAttempts = 6
+    #elseif LIFEDRIVE
+    static let showFAQViewAttempts = 3000
+    static let showSupportViewAttempts = 6000
+    #endif
+    
     
     static let maxDetailsLoadingAttempts = 5
     static let detailsLoadingTimeAwait = UInt32(2)
