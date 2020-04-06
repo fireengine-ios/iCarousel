5003637b483a4c15f7551dfd0251b9ab0d0b69ab
Andrei Novikov <anovikov.brest@gmail.com>
2020-04-06 13:51:05 +0300

    FE-2407 Album Sync - Created album displays duplicate

--- Depo/Depo/Presentation/UserStories/Settings/AutoSync/Configurator/Enteties/AutoSyncModel.swift
+++ Depo/Depo/Presentation/UserStories/Settings/AutoSync/Configurator/Enteties/AutoSyncModel.swift
@@ -140,7 +140,7 @@ final class AutoSyncAlbumModel: AutoSyncModel {
     
     override func equalTo(rhs: AutoSyncModel) -> Bool {
         if let rhs = rhs as? AutoSyncAlbumModel {
-            return album.name == rhs.album.name
+            return album.uuid == rhs.album.uuid
         }
         return super.equalTo(rhs: rhs)
     }
