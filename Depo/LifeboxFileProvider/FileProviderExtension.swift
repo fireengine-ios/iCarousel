eeef136d4cfeaedd3ef702878c6449623363a36e
Maxim Soldatov <maxim.soldatov@me.com>
2020-04-27 13:06:09 +0300

    Deleted NSErrorPointer

--- Depo/LifeboxFileProvider/FileProviderExtension.swift
+++ Depo/LifeboxFileProvider/FileProviderExtension.swift
@@ -95,7 +95,7 @@ final class FileProviderExtension: NSFileProviderExtension {
             }
             
             let placecholderDirectoryUrl = placeholderURL.deletingLastPathComponent()
-            var fcError: NSErrorPointer = NSErrorPointer(nilLiteral: ())
+            var fcError: NSErrorPointer
             
             fileCoordinator.coordinate(writingItemAt: placecholderDirectoryUrl, options: .forMerging, error: fcError) { newURL in
                 
