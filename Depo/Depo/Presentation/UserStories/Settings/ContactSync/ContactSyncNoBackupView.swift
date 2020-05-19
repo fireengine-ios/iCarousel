6d16f369c803eb246965fed35f097181655a97db
Konstantin <>
2020-05-19 15:22:52 +0300

    init

---
+++ Depo/Depo/Presentation/UserStories/Settings/ContactSync/ContactSyncNoBackupView.swift
@@ -0,0 +1,13 @@
+//
+//  ContactSyncNoBackupView.swift
+//  Depo
+//
+//  Created by Konstantin Studilin on 19.05.2020.
+//  Copyright © 2020 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+class ContactSyncNoBackupView: UIView, NibInit {
+
+}
