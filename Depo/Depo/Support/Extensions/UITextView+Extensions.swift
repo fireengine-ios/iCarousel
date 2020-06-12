1ea883a2718ace3df719fbb49b342ba30ed63078
Andrei Novikov <anovikov.brest@gmail.com>
2020-06-12 16:11:02 +0300

    UITextView extension

--- Depo/Depo/Support/Extensions/UITextView+Extensions.swift
+++ Depo/Depo/Support/Extensions/UITextView+Extensions.swift
@@ -1,9 +1,18 @@
 //
-//  UITextView+Extension.swift
+//  UITextView+Extensions.swift
 //  Depo
 //
 //  Created by Andrei Novikau on 6/12/20.
 //  Copyright © 2020 LifeTech. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITextViewDelegate {
+    func defaultHandle(url: URL, interaction: UITextItemInteraction) -> Bool {
+        if interaction == .presentActions {
+            return true
+        }
+        return UIApplication.shared.openSafely(url)
+    }
+}
