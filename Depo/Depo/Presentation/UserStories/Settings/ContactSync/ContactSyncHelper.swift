bbcccd1f072cf37f94bb151eba6eb7d78e22f321
Andrei Novikov <anovikov.brest@gmail.com>
2020-06-23 16:07:45 +0300

    Contact sync - show progress views on viewWillAppear

--- Depo/Depo/Presentation/UserStories/Settings/ContactSync/ContactSyncHelper.swift
+++ Depo/Depo/Presentation/UserStories/Settings/ContactSync/ContactSyncHelper.swift
@@ -43,7 +43,13 @@ final class ContactSyncHelper {
     
     static let shared = ContactSyncHelper()
     
-    weak var delegate: ContactSyncHelperDelegate?
+    weak var delegate: ContactSyncHelperDelegate? {
+        didSet {
+            if let operation = currentOperation, let progressPercentage = currentOperationProgress {
+                delegate?.progress(progress: progressPercentage, for: operation)
+            }
+        }
+    }
     
     private let localContactsService = ContactService()
     private let contactSyncService = ContactsSyncService()
@@ -54,6 +60,11 @@ final class ContactSyncHelper {
     let tokenStorage: TokenStorage = factory.resolve()
     
     private (set) var syncResponse: ContactSync.SyncResponse?
+    
+    private(set) var currentOperation: SyncOperationType?
+    var currentOperationProgress: Int? {
+        return SyncStatus.shared().progress.intValue
+    }
     
     //MARK: - Public
     
@@ -83,7 +94,7 @@ final class ContactSyncHelper {
     func analyze(onStart: @escaping VoidHandler) {
         userHasPermissionFor(type: .deleteDublicate) { [weak self] hasPermission in
             guard hasPermission else {
-                self?.delegate?.didFailed(operationType: .analyze, error: .notPremiumUser)
+                self?.failed(operationType: .analyze, error: .notPremiumUser)
                 return
             }
             self?.startOperation(operationType: .analyze, onStart: onStart)
@@ -93,10 +104,11 @@ final class ContactSyncHelper {
     func deleteDuplicates(onStart: @escaping VoidHandler) {
         userHasPermissionFor(type: .deleteDublicate) { [weak self] hasPermission in
             guard hasPermission else {
-                self?.delegate?.didFailed(operationType: .deleteDuplicated, error: .notPremiumUser)
+                self?.failed(operationType: .deleteDuplicated, error: .notPremiumUser)
                 return
             }
             self?.checkAnalyze()
+            self?.currentOperation = .deleteDuplicated
             self?.contactSyncService.deleteDuplicates()
             onStart()
         }
@@ -115,14 +127,14 @@ final class ContactSyncHelper {
     private func startOperation(operationType: SyncOperationType, onStart: @escaping VoidHandler) {
         requestAccess { [weak self] success in
             guard success else {
-                self?.delegate?.didFailed(operationType: operationType, error: .accessDenied)
+                self?.failed(operationType: operationType, error: .accessDenied)
                 return
             }
             
             switch operationType {
                 case .backup, .analyze:
                     if self?.getStoredContactsCount() == 0 {
-                        self?.delegate?.didFailed(operationType: operationType, error: .emptyStoredContacts)
+                        self?.failed(operationType: operationType, error: .emptyStoredContacts)
                     } else {
                         onStart()
                         self?.proccessOperation(operationType)
@@ -130,7 +142,7 @@ final class ContactSyncHelper {
                 
                 case .restore:
                     if self?.syncResponse?.totalNumberOfContacts == 0  {
-                        self?.delegate?.didFailed(operationType: operationType, error: .emptyLifeboxContacts)
+                        self?.failed(operationType: operationType, error: .emptyLifeboxContacts)
                     } else {
                         onStart()
                         self?.proccessOperation(operationType)
@@ -149,7 +161,7 @@ final class ContactSyncHelper {
     
     private func proccessOperation(_ operationType: SyncOperationType) {
         if !reachability.isReachable && operationType.isContained(in: [.backup, .restore, .analyze]) {
-            delegate?.didFailed(operationType: .deleteDuplicated, error: .syncError(SyncOperationErrors.networkError))
+            failed(operationType: operationType, error: .syncError(SyncOperationErrors.networkError))
             return
         }
         
@@ -164,6 +176,8 @@ final class ContactSyncHelper {
             
             switch result {
             case .success(_):
+                self.currentOperation = operationType
+
                 switch operationType {
                 case .backup:
                     self.contactSyncService.cancelAnalyze()
@@ -188,10 +202,8 @@ final class ContactSyncHelper {
                     assertionFailure("operation is not allowed")
                 }
             case .failed(let error):
-                self.delegate?.didFailed(operationType: operationType, error: .syncError(error))
+                self.failed(operationType: operationType, error: .syncError(error))
             }
-            
-            
         }
     }
     
@@ -199,6 +211,7 @@ final class ContactSyncHelper {
         contactSyncService.analyze(progressCallback: { [weak self] progressPercentage, count, type in
             if progressPercentage == 0 && type == .deleteDuplicated {
                 self?.delegate?.didDeleteDuplicates()
+                self?.currentOperation = nil
             } else {
                 self?.delegate?.progress(progress: progressPercentage, for: type)
             }
@@ -206,6 +219,7 @@ final class ContactSyncHelper {
             debugLog("contactsSyncService.analyze successCallback")
             
             self?.delegate?.didAnalyze(contacts: response)
+            self?.currentOperation = nil
             
         }, cancelCallback: nil,
            errorCallback: { [weak self] errorType, type in
@@ -215,17 +229,20 @@ final class ContactSyncHelper {
     }
     
     private func loadLastBackUp() {
-           contactSyncService.getBackUpStatus(completion: { [weak self] model in
-               debugLog("loadLastBackUp completion")
-               self?.syncResponse = model
-               self?.delegate?.didUpdateBackupStatus()
-               
-           }, fail: { [weak self] in
-               debugLog("loadLastBackUp fail")
-               self?.syncResponse = nil
-               self?.delegate?.didUpdateBackupStatus()
-           })
-       }
+        let handler: ((ContactSync.SyncResponse?) -> Void) = { [weak self] model in
+            self?.syncResponse = model
+            self?.delegate?.didUpdateBackupStatus()
+            self?.currentOperation = nil
+        }
+        
+        contactSyncService.getBackUpStatus(completion: { model in
+            debugLog("loadLastBackUp completion")
+            handler(model)
+        }, fail: {
+            debugLog("loadLastBackUp fail")
+            handler(nil)
+        })
+    }
     
     private func performOperation(forType type: SYNCMode) {
         UIApplication.setIdleTimerDisabled(true)
@@ -241,33 +258,34 @@ final class ContactSyncHelper {
                 debugLog("contactsSyncService.executeOperation finishCallback: \(result)")
                 
                 (type == .backup) ? self?.delegate?.didBackup(result: result) :  self?.delegate?.didRestore()
+                self?.currentOperation = nil
                 
-            }, errorCallback: { [weak self] errorType, opertionType in
+            }, errorCallback: { [weak self] errorType, operationType in
                 self?.analyticsHelper.trackOperationFailure(type: type)
                 
                 debugLog("contactsSyncService.executeOperation errorCallback: \(errorType)")
                 
                 UIApplication.setIdleTimerDisabled(false)
-                self?.delegate?.didFailed(operationType: opertionType, error: .syncError(errorType))
+                self?.failed(operationType: operationType, error: .syncError(errorType))
         })
     }
 
     //MARK:- Tokens + Contacts Access + Persmissions
     
     private func userHasPermissionFor(type: AuthorityType, completion: @escaping BoolHandler) {
-        accountService.permissions { response in
+        accountService.permissions { [weak self] response in
             switch response {
-                case .success(let result):
-                    AuthoritySingleton.shared.refreshStatus(with: result)
-                    completion(result.hasPermissionFor(type))
+            case .success(let result):
+                AuthoritySingleton.shared.refreshStatus(with: result)
+                completion(result.hasPermissionFor(type))
                 
-                case .failed(let error):
-                    if let code = (error as? URLError)?.code, code.isContained(in: [.networkConnectionLost, .notConnectedToInternet]) {
-                        self.delegate?.didFailed(operationType: .deleteDuplicated, error: .syncError(SyncOperationErrors.networkError))
-                        return
-                    }
-
-                    completion(false)
+            case .failed(let error):
+                if let code = (error as? URLError)?.code, code.isContained(in: [.networkConnectionLost, .notConnectedToInternet]) {
+                    self?.failed(operationType: .deleteDuplicated, error: .syncError(SyncOperationErrors.networkError))
+                    return
+                }
+                
+                completion(false)
             }
         }
     }
@@ -291,6 +309,11 @@ final class ContactSyncHelper {
             self?.contactSyncService.updateAccessToken()
             complition(.success(()))
         }
+    }
+    
+    private func failed(operationType: SyncOperationType, error: ContactSyncHelperError) {
+        currentOperation = nil
+        delegate?.didFailed(operationType: operationType, error: error)
     }
 }
 
