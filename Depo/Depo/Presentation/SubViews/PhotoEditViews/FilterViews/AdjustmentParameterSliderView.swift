d8d64d25254557280756280d0c3c485e273737ce
Andrei Novikov <anovikov.brest@gmail.com>
2020-08-13 12:10:37 +0300

    COF-30 Photo Edit - The effect is not always applied if the user changes parameters by clicking on the positive or negative side (not pull)

--- Depo/Depo/Presentation/SubViews/PhotoEditViews/FilterViews/AdjustmentParameterSliderView.swift
+++ Depo/Depo/Presentation/SubViews/PhotoEditViews/FilterViews/AdjustmentParameterSliderView.swift
@@ -15,18 +15,20 @@ class AdjustmentParameterSliderView: UIView {
     
     let slider: MDCSlider = {
         let slider = MDCSlider()
-        slider.filledTrackAnchorValue = 0
         slider.isContinuous = true
         slider.isStatefulAPIEnabled = true
+        slider.isThumbHollowAtStart = false
         slider.setThumbColor(.white, for: .normal)
         slider.setTrackFillColor(.white, for: .normal)
         slider.setTrackBackgroundColor(.gray, for: .normal)
+        slider.addTarget(self, action: #selector(didTouchUpInside(_:)), for: .touchUpInside)
         slider.addTarget(self, action: #selector(didChangeSliderValue(_:)), for: .valueChanged)
         return slider
     }()
     
     private(set) weak var delegate: FilterSliderViewDelegate?
     private(set) var type: AdjustmentParameterType?
+    private var previosValue = CGFloat.greatestFiniteMagnitude
     
     func setup(with parameter: AdjustmentParameterProtocol, delegate: FilterSliderViewDelegate?) {
         backgroundColor = ColorConstants.filterBackColor
@@ -45,10 +47,23 @@ class AdjustmentParameterSliderView: UIView {
     
     //MARK: - Actions
     
+    @objc private func didTouchUpInside(_ sender: MDCSlider) {
+        updateValue(sender.value)
+    }
+    
     @objc private func didChangeSliderValue(_ sender: MDCSlider) {
-        didChangeValue(sender.value)
+        updateValue(sender.value)
+    }
+    
+    private func updateValue(_ value: CGFloat) {
+        guard value != previosValue else {
+            return
+        }
+        
+        previosValue = value
+        didChangeValue(value)
         if let type = type {
-            delegate?.sliderValueChanged(newValue: Float(sender.value), type: type)
+            delegate?.sliderValueChanged(newValue: Float(value), type: type)
         }
     }
     
