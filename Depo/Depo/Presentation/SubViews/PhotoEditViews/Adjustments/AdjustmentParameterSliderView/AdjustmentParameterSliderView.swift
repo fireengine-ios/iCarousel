56d7e0d7eaf17cbfde7bd40eff5691cef3a795b9
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2020-09-17 23:57:40 +0300

    same touch behaviour for prepared filter slider

--- Depo/Depo/Presentation/SubViews/PhotoEditViews/Adjustments/AdjustmentParameterSliderView/AdjustmentParameterSliderView.swift
+++ Depo/Depo/Presentation/SubViews/PhotoEditViews/Adjustments/AdjustmentParameterSliderView/AdjustmentParameterSliderView.swift
@@ -49,6 +49,8 @@ final class AdjustmentParameterSliderView: UIView, NibInit {
     private(set) weak var delegate: AdjustmentParameterSliderViewDelegate?
     private(set) var type: AdjustmentParameterType?
     
+    private var isAvailableToReadTouch = false
+    
     //MARK: - Setup
 
     func setup(with parameter: AdjustmentParameterProtocol, delegate: AdjustmentParameterSliderViewDelegate?) {
@@ -60,51 +62,6 @@ final class AdjustmentParameterSliderView: UIView, NibInit {
         valueLabel.text = String(format: "%.1f", parameter.currentValue)
         
         setupSlider(parameter: parameter)
-    }
-    private var isAvailableToReadTouch = false
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let currentPoint = touch.location(in: self)
-
-            guard let sliderThumb = slider.getThumbView() else {
-                return
-            }
-            
-            let relativeThumbFrame = convert(sliderThumb.frame, from: slider)
-            
-            let enlargedTouchFrame = CGRect(x: relativeThumbFrame.minX - 20, y: relativeThumbFrame.minY - 20, width: relativeThumbFrame.maxX + 20, height: relativeThumbFrame.maxY + 20)
-            if enlargedTouchFrame.contains(currentPoint) {
-                isAvailableToReadTouch = true
-            }
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard isAvailableToReadTouch else {
-            return
-        }
-        
-        let onePercentSlider = slider.frame.maxX / 100
-        
-        if let touch = touches.first {
-            let currentPoint = touch.location(in: self)
-            
-            if currentPoint.x <= slider.frame.minX {
-                slider.slider.setValue(0, animated: true)
-            } else if currentPoint.x >= slider.frame.maxX {
-                slider.slider.setValue(1.0, animated: true)
-            } else {
-                
-                let percentageValue = currentPoint.x / onePercentSlider
-                slider.slider.setValue(percentageValue/100, animated: false)
-            }
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if isAvailableToReadTouch {
-            isAvailableToReadTouch = false
-        }
     }
     
     func setupGradient(startColor: UIColor, endColor: UIColor) {
@@ -134,4 +91,51 @@ final class AdjustmentParameterSliderView: UIView, NibInit {
             self?.delegate?.sliderValueChanged(newValue: value, type: parameter.type)
         }
     }
+}
+
+extension AdjustmentParameterSliderView {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           if let touch = touches.first {
+               let currentPoint = touch.location(in: self)
+
+               guard let sliderThumb = slider.getThumbView() else {
+                   return
+               }
+               
+               let relativeThumbFrame = convert(sliderThumb.frame, from: slider)
+               
+               let enlargedTouchFrame = CGRect(x: relativeThumbFrame.minX - 20, y: relativeThumbFrame.minY - 20, width: relativeThumbFrame.maxX + 20, height: relativeThumbFrame.maxY + 20)
+               if enlargedTouchFrame.contains(currentPoint) {
+                   isAvailableToReadTouch = true
+               }
+           }
+       }
+       
+       override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+           guard isAvailableToReadTouch else {
+               return
+           }
+           
+           let onePercentSlider = slider.frame.maxX / 100
+           
+           if let touch = touches.first {
+               let currentPoint = touch.location(in: self)
+               
+               if currentPoint.x <= slider.frame.minX {
+                   slider.slider.setValue(0, animated: true)
+               } else if currentPoint.x >= slider.frame.maxX {
+                   slider.slider.setValue(1.0, animated: true)
+               } else {
+                   
+                   let percentageValue = currentPoint.x / onePercentSlider
+                   slider.slider.setValue(percentageValue/100, animated: false)
+               }
+           }
+       }
+       
+       override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+           if isAvailableToReadTouch {
+               isAvailableToReadTouch = false
+           }
+       }
 }
