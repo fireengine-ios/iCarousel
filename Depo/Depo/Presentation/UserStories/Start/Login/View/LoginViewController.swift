402851ef57598986b65aec700158350c66ffb38b
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2020-10-06 17:40:53 +0300

    login creds are back

--- Depo/Depo/Presentation/UserStories/Start/Login/View/LoginViewController.swift
+++ Depo/Depo/Presentation/UserStories/Start/Login/View/LoginViewController.swift
@@ -142,9 +142,9 @@ final class LoginViewController: ViewController {
         output.viewIsReady()
         
         #if DEBUG
-        loginEnterView.textField.text = "+375259091520"
+        loginEnterView.textField.text = "mavokij291@4tmail.com"//"qwerty@my.com"// "test3@test.test"//"test2@test.test"//"testasdasdMail@notRealMail.yep"
 
-        passwordEnterView.textField.text = ".FsddQ646"
+        passwordEnterView.textField.text = "qwerty"// "zxcvbn"//".FsddQ646"
         #endif
     }
     
