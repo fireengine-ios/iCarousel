6ac57b2802a8b6017abf6113981ec96bc47995d2
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2020-10-13 01:44:50 +0300

    removed wormhole for provider

--- Depo/LifeboxWidget/WidgetConfiguration/WidgetProvider.swift
+++ Depo/LifeboxWidget/WidgetConfiguration/WidgetProvider.swift
@@ -9,8 +9,6 @@
 import Foundation
 import SwiftUI
 import WidgetKit
-import MMWormhole
-
 
 typealias WidgetBaseEntriesCallback = ([WidgetBaseEntry]) -> ()
 typealias WidgetBaseEntryCallback = (WidgetBaseEntry?) -> ()
