7f458e67327c86af9e1cb7ddf500ac96e3d4438d
Andrei Novikov <anovikov.brest@gmail.com>
2020-11-06 10:21:55 +0300

    COF-579 File Upload Improvements (Trash bin) - Inside of a folder, plus button is clickable

--- Depo/Depo/Presentation/UserStories/Grid/BaseFilesGreed/Configurator/BaseFilesGreedInitializer.swift
+++ Depo/Depo/Presentation/UserStories/Grid/BaseFilesGreed/Configurator/BaseFilesGreedInitializer.swift
@@ -144,7 +144,9 @@ class BaseFilesGreedModuleInitializer: NSObject {
     class func initializeFilesFromFolderViewController(with nibName: String, folder: Item, type: MoreActionsConfig.ViewType, sortType: MoreActionsConfig.SortRullesType, status: ItemStatus, moduleOutput: BaseFilesGreedModuleOutput?, alertSheetExcludeTypes: [ElementTypes]? = nil) -> UIViewController {
         let viewController = BaseFilesGreedChildrenViewController(nibName: nibName, bundle: nil)
         viewController.needToShowTabBar = true
-        viewController.floatingButtonsArray.append(contentsOf: [.uploadFiles, .newFolder, .uploadFromLifebox])
+        if status == .active {
+            viewController.floatingButtonsArray.append(contentsOf: [.uploadFiles, .newFolder, .uploadFromLifebox])
+        }
         viewController.scrollablePopUpView.addPermittedPopUpViewTypes(types: [.sync, .upload, .download])
         viewController.scrollablePopUpView.isEnable = true
         viewController.status = status
