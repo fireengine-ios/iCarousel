4ed32ce28edb6a393c132e6618ffcb492a7b0963
Andrei Novikov <anovikov.brest@gmail.com>
2020-11-10 12:22:25 +0300

    COF-520 Remote suggestions API works

--- Depo/Depo/Business/ApiServices/PrivateShare/PrivateShareApiService.swift
+++ Depo/Depo/Business/ApiServices/PrivateShare/PrivateShareApiService.swift
@@ -16,10 +16,6 @@ final class PrivateShareApiServiceImpl: PrivateShareApiService {
     
     @discardableResult
     func getSuggestions(handler: @escaping ResponseArrayHandler<SuggestedApiContact>) -> URLSessionTask? {
-        //TODO: remove when API will working
-        handler(.success(SuggestedApiContact.testContacts()))
-        return nil
-        
         return SessionManager
             .customDefault
             .request(RouteRequests.PrivateShare.suggestions)
