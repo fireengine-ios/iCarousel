ca8a193ac9ca6ac30b34db77b4530ca65bb833ab
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2020-11-17 10:00:40 +0300

    missing assets + redeclaration removed

--- Depo/Depo/Presentation/UserStories/Others/PrivateShare/PrivateShareViewController.swift
+++ Depo/Depo/Presentation/UserStories/Others/PrivateShare/PrivateShareViewController.swift
@@ -314,13 +314,7 @@ extension PrivateShareViewController: PrivateShareSelectPeopleViewDelegate {
         endSearchContacts()
         updateShareButtonIfNeeded()
     }
-    
-    private func isValidContact(text: String) -> Bool {
-        if Validator.isValid(email: text) || Validator.isValid(contactsPhone: text) {
-            return true
-        }
-        return false
-    }
+
 }
 
 //MARK: - PrivateShareWithViewDelegate
