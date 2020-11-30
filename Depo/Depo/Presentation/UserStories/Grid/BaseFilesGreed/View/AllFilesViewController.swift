06643914b52257de7ea7879e0d31cdfe78aabd34
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2020-11-30 13:08:38 +0300

    COF-677 - all files shared slider refresh

--- Depo/Depo/Presentation/UserStories/Grid/BaseFilesGreed/View/AllFilesViewController.swift
+++ Depo/Depo/Presentation/UserStories/Grid/BaseFilesGreed/View/AllFilesViewController.swift
@@ -9,47 +9,47 @@
 final class AllFilesViewController: BaseFilesGreedChildrenViewController {
     
     private let sharedFilesManager = SharedFilesCollectionManager()
-    private let sharedSliderHeight: CGFloat = 224
+    
+    private var isSliderSetuped = false
+    
+    private var lastCardContainerHeight: CGFloat = 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        handleShareSliderReload()
+    }
     
     override func setupInitialState() {
         super.setupInitialState()
-        checkPrivateShareSliderAvailability()
+        setupInitialPrivateShareSlider()
     }
     
-    private func checkPrivateShareSliderAvailability() {
-        sharedFilesManager.checkSharedWithMe { [weak self] result in
-            guard let self = self else {
-                return
-            }
-            switch result {
-            case .success(_):
-                DispatchQueue.main.async {
-                    self.addPrivateShareSlider()
-                }
-            case .failed(_):
-                break
-            }
-        }
-    }
-    
-    private func addPrivateShareSlider() {
+    private func setupInitialPrivateShareSlider() {
         
         let containerView = sharedFilesManager.sharedFilesSlider
         collectionView.addSubview(containerView)
         
         containerView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.translatesAutoresizingMaskIntoConstraints = false
+        sharedFilesManager.delegate = self
+        sharedFilesManager.changeSliderVisability(isHidden: true)
         
         var constraintsArray = [NSLayoutConstraint]()
-
-        let height = cardsContainerView.frame.size.height + containerView.frame.size.height
-      
-        self.collectionView.contentInset = UIEdgeInsets(top: height, left: 0, bottom: 25, right: 0)
-        let topOffset = CGPoint(x: 0, y: 0 - height)
-        collectionView.setContentOffset(topOffset , animated: false)
-        collectionView.updateConstraints()
-
         constraintsArray.append(NSLayoutConstraint(item: containerView, attribute: .bottom, relatedBy: .equal, toItem: collectionView, attribute: .top, multiplier: 1, constant: 0))
+        constraintsArray.append(NSLayoutConstraint(item: containerView, attribute: .centerX, relatedBy: .equal, toItem: collectionView, attribute: .centerX, multiplier: 1, constant: 0))
+        constraintsArray.append(NSLayoutConstraint(item: containerView, attribute: .width, relatedBy: .equal, toItem: collectionView, attribute: .width, multiplier: 1, constant: 0))
+        
+        NSLayoutConstraint.activate(constraintsArray)
+        
+        isSliderSetuped = true
+    }
+    
+    private func refreshSharedSliderPosition() {
+        let height: CGFloat = lastCardContainerHeight + sharedFilesManager.sharedFilesSlider.frame.size.height
+        
+        collectionView.contentInset = UIEdgeInsets(top: height, left: 0, bottom: 25, right: 0)
+        let topOffset = CGPoint(x: 0, y: -height)
+        collectionView.setContentOffset(topOffset , animated: false)
         
         if let yConstr = self.contentSliderTopY {
             yConstr.constant = -height
@@ -57,27 +57,26 @@ final class AllFilesViewController: BaseFilesGreedChildrenViewController {
         if let hConstr = self.contentSliderH {
             hConstr.constant = height
         }
-
-        collectionView.updateConstraints()
         
-        constraintsArray.append(NSLayoutConstraint(item: containerView, attribute: .centerX, relatedBy: .equal, toItem: collectionView, attribute: .centerX, multiplier: 1, constant: 0))
-        constraintsArray.append(NSLayoutConstraint(item: containerView, attribute: .width, relatedBy: .equal, toItem: collectionView, attribute: .width, multiplier: 1, constant: 0))
-        
-        refresherY =  -height + 30
+        refresherY = -height + 30
         updateRefresher()
         
         noFilesViewCenterOffsetConstraint.constant = BaseFilesGreedViewController.sliderH / 2
         
-        NSLayoutConstraint.activate(constraintsArray)
-        sharedFilesManager.delegate = self
-        
+        collectionView.updateConstraints()
         view.layoutSubviews()
+        
     }
     
     override func onUpdateViewForPopUpH(h: CGFloat) {
-
+        lastCardContainerHeight = h
+        
         let originalPoint = collectionView.contentOffset
-        let sliderH: CGFloat =   sharedFilesManager.sharedFilesSlider.frame.size.height
+        var sliderH: CGFloat =   0
+        
+        if !sharedFilesManager.sharedFilesSlider.isHidden {
+            sliderH = sharedFilesManager.sharedFilesSlider.frame.size.height
+        }
         
         let calculatedH = h + sliderH
         
@@ -86,7 +85,7 @@ final class AllFilesViewController: BaseFilesGreedChildrenViewController {
                 yConstr.constant = -calculatedH
             }
             if let hConstr = self.contentSliderH {
-                hConstr.constant = calculatedH//h
+                hConstr.constant = calculatedH
             }
             
             self.view.layoutIfNeeded()
@@ -105,5 +104,38 @@ final class AllFilesViewController: BaseFilesGreedChildrenViewController {
         
         refresherY = -calculatedH + 30
         updateRefresher()
+    }
+    
+    @objc override func loadData() {
+        guard isRefreshAllowed else {
+            return
+        }
+        if !output.isSelectionState() {
+            output.onReloadData()
+            contentSlider?.reloadAllData()
+            handleShareSliderReload()
+        } else {
+            refresher.endRefreshing()
+        }
+    }
+    
+    private func handleShareSliderReload() {
+        guard isSliderSetuped else {
+            return
+        }
+        sharedFilesManager.reloadData { [weak self] result in
+            guard let self = self else {
+                return
+            }
+            DispatchQueue.main.async {
+                switch result {
+                case .success(_):
+                    self.sharedFilesManager.changeSliderVisability(isHidden: false)
+                case .failed(_):
+                    self.sharedFilesManager.changeSliderVisability(isHidden: true)
+                }
+                self.refreshSharedSliderPosition()
+            }
+        }
     }
 }
