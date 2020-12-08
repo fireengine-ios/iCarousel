a07caf4e342ac4c52945d0d69eab987e08a6cfa5
Andrei Novikov <anovikov.brest@gmail.com>
2020-12-08 15:08:15 +0300

    COF-756 Private Share - Message Area Limit

--- Depo/Depo/Presentation/UserStories/Others/PrivateShare/New Share/Views/PrivateShareAddMessageView.swift
+++ Depo/Depo/Presentation/UserStories/Others/PrivateShare/New Share/Views/PrivateShareAddMessageView.swift
@@ -26,6 +26,7 @@ final class PrivateShareAddMessageView: UIView, NibInit {
             newValue.contentInset = .zero
             newValue.isScrollEnabled = false
             newValue.inputAccessoryView = toolbar
+            newValue.delegate = self
         }
     }
     
@@ -51,11 +52,32 @@ final class PrivateShareAddMessageView: UIView, NibInit {
         return toolbar
     }()
     
+    private let messageLengthLimit = 500
+    
     var message: String {
         textView.text
     }
     
     @objc private func hideKeyboard() {
         textView.resignFirstResponder()
+    }
+}
+
+extension PrivateShareAddMessageView: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        guard let stringRange = Range(range, in: textView.text) else {
+            return false
+        }
+        
+        let changedText = textView.text.replacingCharacters(in: stringRange, with: text)
+        if changedText.utf16.count > messageLengthLimit {
+            //insert the maximum possible number of letters
+            let index = String.Index(utf16Offset: messageLengthLimit, in: changedText)
+            textView.text = String(changedText[...index])
+            return false
+        }
+        
+        return true
     }
 }
