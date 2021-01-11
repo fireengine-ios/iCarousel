d3a1e52aa0d6c067f15facf866f2e0284d70f9b7
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2021-01-11 10:59:32 +0300

    update user on view did load

--- Depo/LifeBox_Business/Presentation/SubViews/CustomTabBars/TabBarViewController.swift
+++ Depo/LifeBox_Business/Presentation/SubViews/CustomTabBars/TabBarViewController.swift
@@ -129,9 +129,7 @@ final class TabBarViewController: ViewController, UITabBarDelegate {
         
         plussButton.accessibilityLabel = TextConstants.accessibilityPlus
         
-        #if LIFEDRIVE
-        plussButton.imageEdgeInsets = UIEdgeInsets(top: -15, left: -15, bottom: -15, right: -15)
-        #endif
+        AnalyticsService.updateUser()
     }
     
     deinit {
