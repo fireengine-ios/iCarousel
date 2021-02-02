bcc10db19a35f80879e9392f55fb90c400ac0fa4
Konstantin <>
2021-02-02 14:32:25 +0300

    new cell

--- Depo/LifeBox_Business/Presentation/Cells/MultifileCollectionViewCell/MultifileCollectionViewCell.swift
+++ Depo/LifeBox_Business/Presentation/Cells/MultifileCollectionViewCell/MultifileCollectionViewCell.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class MultifileCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var thumbnail: UIImageView! {
+        willSet {
+            newValue.contentMode = .scaleAspectFit
+        }
+    }
+    
+    @IBOutlet private weak var name: UILabel! {
+        willSet {
+            newValue.font = UIFont.TurkcellSaturaRegFont(size: 14.0)
+//            newValue.textColor = 
+        }
+    }
+    @IBOutlet private weak var lastModifiedDate: UILabel!
+    @IBOutlet private weak var iconsStack: UIStackView!
+    @IBOutlet private weak var menuButton: UIButton!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
 }
