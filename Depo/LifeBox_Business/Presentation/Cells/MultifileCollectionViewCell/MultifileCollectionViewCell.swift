4e2733247cd65943702022dd6c113e0007f5cc57
Konstantin <>
2021-02-09 09:45:01 +0300

    renaming ui

--- Depo/LifeBox_Business/Presentation/Cells/MultifileCollectionViewCell/MultifileCollectionViewCell.swift
+++ Depo/LifeBox_Business/Presentation/Cells/MultifileCollectionViewCell/MultifileCollectionViewCell.swift
@@ -74,16 +74,38 @@ class MultifileCollectionViewCell: UICollectionViewCell {
     }()
     
     
+    //MARK: NAme editing
     @IBOutlet weak var nameEditView: UIView! {
         willSet {
             newValue.alpha = 0
         }
     }
     
+    @IBOutlet weak var renameField: UITextField! {
+        willSet {
+            newValue.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var cancelRenamingButton: UIButton! {
+        willSet {
+            newValue.titleLabel?.text = ""
+            let cancelImage = UIImage(named: "cancelButton")
+            newValue.setImage(cancelImage, for: .normal)
+        }
+    }
+    
+    @IBOutlet weak var applyRenamingButton: UIButton! {
+        willSet {
+            newValue.titleLabel?.text = ""
+            let cancelImage = UIImage(named: "applyButton")
+            newValue.setImage(cancelImage, for: .normal)
+        }
+    }
+    
     private var itemModel : Item?
-    
     private var isAllowedToShowShared: Bool = false
-    
+    private var pathExtensionLength: Int = 0
     
     override var isSelected: Bool {
         willSet {
@@ -109,6 +131,8 @@ class MultifileCollectionViewCell: UICollectionViewCell {
         
         name.text = ""
         lastModifiedDate.text = ""
+        renameField.text = ""
+        pathExtensionLength = 0
         thumbnail.image = nil
         iconsStack.arrangedSubviews.forEach { $0.removeFromSuperview() }
         selectIconWidth.constant = 0
@@ -124,6 +148,7 @@ class MultifileCollectionViewCell: UICollectionViewCell {
     
     func setup(with item: Item, at indexPath: IndexPath, isSharedIconAllowed: Bool, menuActionDelegate: MultifileCollectionViewCellActionDelegate?) {
         itemModel = item
+        pathExtensionLength = (item.name as NSString?)?.pathExtension.count ?? 0
         isAllowedToShowShared = isSharedIconAllowed
         actionDelegate = menuActionDelegate
         
@@ -155,7 +180,7 @@ class MultifileCollectionViewCell: UICollectionViewCell {
     }
     
     func setSelection(isSelectionActive: Bool, isSelected: Bool) {
-        self.isSelected = isSelected
+        self.isSelected = isSelectionActive && isSelected
         
         let widthConstant: CGFloat
         
@@ -172,6 +197,10 @@ class MultifileCollectionViewCell: UICollectionViewCell {
         } completion: { _ in
             self.layoutIfNeeded()
         }
+        
+        if #available(iOS 14.0, *) {
+            setMenu(isAvailable: !isSelectionActive)
+        }
 
     }
     
@@ -187,14 +216,63 @@ class MultifileCollectionViewCell: UICollectionViewCell {
     }
     
     @objc private func onLongTap(_ sender: Any) {
+        
+        //TODO: handle renaming
+        
         if #available(iOS 14.0, *) {
-            //use button + UIMenu instead
+            //using button + UIMenu instead
             return
             
         } else {
             actionDelegate?.onMenuPress(sender: sender, itemModel: itemModel)
         }
     }
+    
+    private func showRenamingView() {
+        if #available(iOS 14.0, *) {
+            menuButton.isUserInteractionEnabled = false
+        }
+        
+        renameField.text = self.itemModel?.name
+        renameField.becomeFirstResponder()
+        
+        UIView.animate(withDuration: NumericConstants.animationDuration, delay: 0, options: [.curveEaseInOut]) {
+            self.nameEditView.alpha = 1
+            
+        } completion: { _ in
+            
+            let offset = (self.renameField.text?.count ?? 0) - self.pathExtensionLength - 1
+            if let position = self.renameField.position(from: self.renameField.beginningOfDocument, offset: offset) {
+                self.renameField.selectedTextRange = self.renameField.textRange(from: position, to: position)
+            }
+        }
+    }
+    
+    private func hideRenamignView() {
+        
+        if #available(iOS 14.0, *) {
+            self.menuButton.isUserInteractionEnabled = true
+        }
+        
+        UIView.animate(withDuration: NumericConstants.animationDuration, delay: 0, options: [.curveEaseInOut]) {
+            self.nameEditView.alpha = 0
+            
+        } completion: { _ in
+            //
+        }
+    }
+    
+    @IBAction func stopRenaming(_ sender: Any) {
+        hideRenamignView()
+        //
+    }
+    
+    
+    @IBAction func applyRenaming(_ sender: Any) {
+        hideRenamignView()
+        //
+    }
+    
 }
 
 @available(iOS 14, *)
@@ -217,10 +295,7 @@ extension MultifileCollectionViewCell {
         
         let menu = MenuItemsFabric.generateMenu(for: item, status: item.status) { [weak self] actionType in
             if case .elementType(.rename) = actionType {
-                UIView.animate(withDuration: NumericConstants.animationDuration, delay: 0, options: [.curveEaseInOut]) {
-                    self?.nameEditView.alpha = 1
-                } completion: { _ in }
-
+                self?.showRenamingView()
             } else {
                 self?.actionDelegate?.onSelectMenuAction(type: actionType, itemModel: self?.itemModel, sender: self?.menuButton)
             }
@@ -229,12 +304,30 @@ extension MultifileCollectionViewCell {
         menuButton.menu = menu
     }
     
-    @objc func onCellSelected(_ sender: Any) {
+    private func setMenu(isAvailable: Bool) {
+        if isAvailable, let indexPath = menuButton.indexPath {
+            setupMenu(indexPath: indexPath)
+        } else {
+            menuButton.menu = nil
+        }
+    }
+    
+    @objc private func onCellSelected(_ sender: Any) {
         guard let button = sender as? IndexPathButton, let indexPath = button.indexPath else {
             return
         }
         
         actionDelegate?.onCellSelected(indexPath: indexPath)
+    }
+}
+
+
+extension MultifileCollectionViewCell: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let textLength = textField.text?.count, pathExtensionLength > 0 else {
+            return true
+        }
+        return range.upperBound < textLength - pathExtensionLength
     }
 }
 
