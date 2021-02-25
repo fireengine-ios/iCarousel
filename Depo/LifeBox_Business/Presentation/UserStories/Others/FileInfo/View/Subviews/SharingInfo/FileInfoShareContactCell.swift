38b5923d8d205279a4e506f0bf7f4c33d5ee3a99
Anton Ignatovich <Anton.Ignatovich@life.com.by>
2021-02-25 13:36:45 +0300

    configuration of shared to contact cell + additional refactoring

--- Depo/LifeBox_Business/Presentation/UserStories/Others/FileInfo/View/Subviews/SharingInfo/FileInfoShareContactCell.swift
+++ Depo/LifeBox_Business/Presentation/UserStories/Others/FileInfo/View/Subviews/SharingInfo/FileInfoShareContactCell.swift
@@ -31,7 +31,7 @@ final class FileInfoShareContactCell: UICollectionViewCell {
     
     @IBOutlet private weak var button: UIButton! {
         willSet {
-            newValue.titleLabel?.font = .TurkcellSaturaDemFont(size: 18)
+            newValue.titleLabel?.font = UIFont.GTAmericaStandardRegularFont(size: 15)
             newValue.titleLabel?.textAlignment = .center
         }
     }
@@ -39,8 +39,8 @@ final class FileInfoShareContactCell: UICollectionViewCell {
     @IBOutlet private weak var roleLabel: UILabel! {
         willSet {
             newValue.text = ""
-            newValue.textColor = .lrGreyishBrownThree
-            newValue.font = .TurkcellSaturaRegFont(size: 14)
+            newValue.textColor = ColorConstants.infoPageValueText
+            newValue.font = UIFont.GTAmericaStandardRegularFont(size: 14)
             newValue.textAlignment = .center
             newValue.adjustsFontSizeToFitWidth = true
             newValue.minimumScaleFactor = 0.7
@@ -62,51 +62,47 @@ final class FileInfoShareContactCell: UICollectionViewCell {
         button.backgroundColor = .clear
         circleView.layer.borderWidth = 0
         roleLabel.text = ""
+        button.titleLabel?.font = UIFont.GTAmericaStandardRegularFont(size: 14)
+        button.titleEdgeInsets = .zero
     }
     
-    func setup(type: FileInfoShareContactCellType, contact: SharedContact?, count: Int?, index: Int) {
+    func setup(type: FileInfoShareContactCellType,
+               contact: SharedContact?,
+               count: Int?,
+               index: Int) {
         self.type = type
         self.contact = contact
 
         switch type {
         case .contact:
-            
-            func setupInitials() {
-                if let initials = contact?.initials, !initials.isEmpty {
-                    button.setTitle(initials, for: .normal)
-                    button.backgroundColor = contact?.color(for: index) ?? .clear
-                } else {
-                    button.setImage(UIImage(named: "contact_placeholder"), for: .normal)
-                }
+            button.titleEdgeInsets = UIEdgeInsets(top: 2, left: 0, bottom: 0, right: 0)
+            if let initials = contact?.initials, !initials.isEmpty {
+                button.setTitle(initials, for: .normal)
             }
+            button.backgroundColor = circleBackgroundColor(for: index)
             
-            if let url = contact?.subject?.picture?.byTrimmingQuery {
-                button.setImage(UIImage(named: "contact_placeholder"), for: .normal)
-                imageDownloder.getImage(patch: url) { [weak self] image in
-                    if image == nil {
-                        setupInitials()
-                    } else {
-                        self?.button.setImage(image, for: .normal)
-                    }
-                }
-            } else {
-                setupInitials()
-            }
-            
-            button.setTitleColor(.white, for: .normal)
-            roleLabel.text = contact?.role.infoMenuTitle ?? ""
+            button.setTitleColor(onCircleTextColor(for: index), for: .normal)
+            roleLabel.text = contact?.role.infoMenuTitle
             
         case .additionalCount:
             if let count = count {
-                button.setTitle("+\(count)", for: .normal)
+                if count < 1000 {
+                    button.setTitle("+ \(count)", for: .normal)
+                } else {
+                    button.setTitle("+ \(count / 1000)k", for: .normal)
+                }
+            }
+
+            if let count = count, count > 99, count < 1000 {
+                button.titleLabel?.font = UIFont.GTAmericaStandardRegularFont(size: 12)
             }
             
-            button.setTitleColor(ColorConstants.marineFour, for: .normal)
-            circleView.layer.borderWidth = 2
-            circleView.layer.borderColor = ColorConstants.marineFour.cgColor
+            button.setTitleColor(ColorConstants.infoPagePlusButtonText, for: .normal)
+            circleView.layer.borderWidth = 1
+            circleView.layer.borderColor = ColorConstants.infoPagePlusButtonText.cgColor
             
         case .plusButton:
-            button.setImage(UIImage(named: "plus_large"), for: .normal)
+            button.setImage(UIImage(named: "info_contact_add"), for: .normal)
         }
     }
     
@@ -120,6 +116,30 @@ final class FileInfoShareContactCell: UICollectionViewCell {
             delegate?.didTappedOnShowAllContacts()
         case .plusButton:
             delegate?.didTappedPlusButton()
+        }
+    }
+}
+
+extension FileInfoShareContactCell {
+    private func circleBackgroundColor(for index: Int) -> UIColor? {
+        switch index.remainderReportingOverflow(dividingBy: 6).partialValue {
+        case 0, 3:
+            return ColorConstants.infoPageContactDarkBackground
+        case 1, 2:
+            return ColorConstants.infoPageContactLigherBackground
+        default:
+            return nil
+        }
+    }
+
+    private func onCircleTextColor(for index: Int) -> UIColor? {
+        switch index.remainderReportingOverflow(dividingBy: 6).partialValue {
+        case 0, 3:
+            return ColorConstants.infoPageDarkerNickname
+        case 1, 2:
+            return ColorConstants.infoPageLigherNickname
+        default:
+            return nil
         }
     }
 }
