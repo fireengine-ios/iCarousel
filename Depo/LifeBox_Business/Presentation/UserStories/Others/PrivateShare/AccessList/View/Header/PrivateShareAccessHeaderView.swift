3f8e5e52fb7a749951d8af2532aa70fe7f2fac43
Anton Ignatovich <Anton.Ignatovich@life.com.by>
2021-03-01 13:21:02 +0300

    added access page header view

---
+++ Depo/LifeBox_Business/Presentation/UserStories/Others/PrivateShare/AccessList/View/Header/PrivateShareAccessHeaderView.swift
@@ -0,0 +1,70 @@
+//
+//  PrivateShareAccessHeaderView.swift
+//  Depo
+//
+//  Created by Anton Ignatovich on 01.03.2021.
+//  Copyright © 2021 LifeTech. All rights reserved.
+//
+
+import Foundation
+
+final class PrivateShareAccessHeaderView: UIView {
+    private struct Constants {
+        static let leadingTrailingOffset: CGFloat = 20
+        static let topOffset: CGFloat = 30
+        static let emailToLabelVerticalOffset: CGFloat = 5
+        static let bottomOffset: CGFloat = 15
+    }
+
+    private lazy var nameLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.GTAmericaStandardRegularFont(size: 14)
+        lbl.textColor = ColorConstants.infoPageValueText
+        return lbl
+    }()
+
+    private lazy var emailLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.GTAmericaStandardRegularFont(size: 14)
+        lbl.textColor = ColorConstants.sharedContactTitleSubtitle
+        return lbl
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stk = UIStackView()
+        stk.translatesAutoresizingMaskIntoConstraints = false
+        stk.axis = .vertical
+        stk.spacing = Constants.emailToLabelVerticalOffset
+        stk.backgroundColor = .clear
+        return stk
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    private func setup() {
+        addSubview(stackView)
+
+        stackView.pinToSuperviewEdges(offset: UIEdgeInsets(top: Constants.topOffset,
+                                                       left: Constants.leadingTrailingOffset,
+                                                       bottom: Constants.bottomOffset,
+                                                       right: Constants.leadingTrailingOffset))
+
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(emailLabel)
+    }
+
+    func updateTexts(name: String?, email: String?) {
+        nameLabel.text = name
+        emailLabel.text = email
+    }
+}
