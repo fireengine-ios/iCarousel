64e7c8bc1988397120122dcd1a2ec93ca214864c
Konstantin <>
2021-03-04 12:34:14 +0300

    new controller

---
+++ Depo/LifeBox_Business/Presentation/UserStories/Others/Upload/UploadSelectionListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UploadSelectionListViewController.swift
+//  Depo
+//
+//  Created by Konstantin Studilin on 04.03.2021.
+//  Copyright © 2021 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+class UploadSelectionListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
