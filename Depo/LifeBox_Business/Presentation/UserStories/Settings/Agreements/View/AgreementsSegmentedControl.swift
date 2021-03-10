48912e59da4e424f855c310a4b40ccd2de461fd2
Vyacheslav_Bakinskiy <vyacheslav.bakinskiy@life.com.by>
2021-03-10 12:59:29 +0300

    COF-1138 Added custom segmented control

---
+++ Depo/LifeBox_Business/Presentation/UserStories/Settings/Agreements/View/AgreementsSegmentedControl.swift
@@ -0,0 +1,95 @@
+//
+//  AgreementsSegmentedControl.swift
+//  Depo
+//
+//  Created by Vyacheslav Bakinskiy on 10.03.21.
+//  Copyright © 2021 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+protocol AgreementsSegmentedControlDelegate: class {
+    func segmentedControlButton(didChangeIndexTo index: Int)
+}
+
+class AgreementsSegmentedControl: UIView {
+    
+    weak var delegate: AgreementsSegmentedControlDelegate?
+    
+    private var buttonTitles: [String]!
+    private var buttons: [UIButton]!
+    private var selectorView: UIView!
+    
+    var textColor: UIColor = ColorConstants.multifileCellSubtitleText
+    var selectorViewColor: UIColor = ColorConstants.confirmationPopupTitle
+    var selectorTextColor: UIColor = ColorConstants.confirmationPopupTitle
+    
+    convenience init(frame: CGRect, buttonTitles: [String]) {
+        self.init(frame: frame)
+        self.buttonTitles = buttonTitles
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        updateView()
+    }
+    
+    func setButtonTitles(buttonTitles: [String]) {
+        self.buttonTitles = buttonTitles
+        updateView()
+    }
+    
+    private func createButton() {
+        buttons = [UIButton]()
+        buttons.removeAll()
+        subviews.forEach ({ $0.removeFromSuperview() })
+        
+        for buttonTitle in buttonTitles {
+            let button = UIButton(type: .system)
+            button.setTitle(buttonTitle, for: .normal)
+            button.addTarget(self, action: #selector(AgreementsSegmentedControl.buttonAction(sender:)), for: .touchUpInside)
+            button.setTitleColor(textColor, for: .normal)
+            buttons.append(button)
+        }
+    }
+    
+    private func configStackView() {
+        let stack = UIStackView(arrangedSubviews: buttons)
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.distribution = .fillEqually
+        addSubview(stack)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stack.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stack.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        stack.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    private func configSelectorView() {
+        let selectorWidth = frame.width / CGFloat(self.buttonTitles.count)
+        selectorView = UIView(frame: CGRect(x: 0, y: self.frame.height, width: selectorWidth, height: 1))
+        selectorView.backgroundColor = selectorViewColor
+        addSubview(selectorView)
+    }
+    
+    private func updateView() {
+        createButton()
+        configStackView()
+        configSelectorView()
+    }
+    
+    @objc func buttonAction(sender: UIButton) {
+        for (buttonIndex, btn) in buttons.enumerated() {
+            btn.setTitleColor(textColor, for: .normal)
+            if btn == sender {
+                let selectorPosition = frame.width / CGFloat(buttonTitles.count) * CGFloat(buttonIndex)
+                delegate?.segmentedControlButton(didChangeIndexTo: buttonIndex)
+                UIView.animate(withDuration: 0.3) {
+                    self.selectorView.frame.origin.x = selectorPosition
+                }
+                btn.setTitleColor(selectorTextColor, for: .normal)
+            }
+        }
+    }
+}
---
+++ Depo/LifeBox_Business/Presentation/UserStories/Settings/Agreements/View/AgreementsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AgreementsViewController.swift
+//  Depo_LifeTech
+//
+//  Created by Vyacheslav Bakinskiy on 10.03.21.
+//  Copyright © 2021 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+class AgreementsViewController: BaseViewController, NibInit {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTitle(withString: TextConstants.agreements)
+        setSegmentedControl()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if !Device.isIpad {
+            defaultNavBarStyle()
+        }
+    }
+    
+    private func setSegmentedControl() {
+        let segmentedControl = AgreementsSegmentedControl(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 43), buttonTitles: ["Terms of Use", "Privacy Policy"])
+        segmentedControl.backgroundColor = .clear
+        segmentedControl.delegate = self
+        view.addSubview(segmentedControl)
+    }
+}
+
+extension AgreementsViewController: AgreementsSegmentedControlDelegate {
+    func segmentedControlButton(didChangeIndexTo index: Int) {
+        switch index {
+        case 0:
+            print("===============> 1")
+        case 1:
+            print("===============> 2")
+        default:
+            print("===============> 3")
+        }
+    }
+}
