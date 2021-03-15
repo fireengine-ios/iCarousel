59b77aa94f748e78bba32747ec40d40ac116d7a4
Anton Ignatovich <Anton.Ignatovich@life.com.by>
2021-03-15 14:14:22 +0300

    imlemented view controller for settings screen + refactored all required outputs etc. for settings screen

--- Depo/LifeBox_Business/Presentation/UserStories/Settings/Settings/View/SettingsViewOutput.swift
+++ Depo/LifeBox_Business/Presentation/UserStories/Settings/Settings/View/SettingsViewOutput.swift
@@ -10,44 +10,11 @@ protocol SettingsViewOutput {
 
     func viewIsReady()
     
-    func viewWillBecomeActive()
-    
     func onLogout()
     
     func goToHelpAndSupport()
     
     func goToTermsAndPolicy()
-        
-    func goToPermissions()
-    //Photo related methods - below
-    func onChangeUserPhoto(quotaInfo: QuotaInfoResponse?)
-        
-    func onChooseFromPhotoLibriary<T: UIViewController>(onViewController viewController: T) where T: UIImagePickerControllerDelegate & UINavigationControllerDelegate
-    func onChooseFromPhotoCamera<T: UIViewController>(onViewController viewController: T) where T: UIImagePickerControllerDelegate & UINavigationControllerDelegate
-    
-    func photoCaptured(data: Data)
-    
-    func goToActivityTimeline()
- 
-    func goToPasscodeSettings(needReplaceOfCurrentController: Bool)
-    
-    func openPasscode(handler: @escaping VoidHandler)
-    
-    func goTurkcellSecurity()
-    
-    func goToMyProfile(userInfo: AccountInfoResponse)
     
     func presentErrorMessage(errorMessage: String)
-    
-    func presentActionSheet(alertController: UIAlertController)
-    
-    var isPasscodeEmpty: Bool { get }
-    
-    var isPremiumUser: Bool { get }
-
-//    func turkcellSecurityStatusNeeded(passcode: Bool, autoLogin: Bool)
-//    func turkcellSecurityChanged(passcode: Bool, autoLogin: Bool)
-//
-    var isMailRequired: Bool { get }
-    var isTurkCellUser: Bool { get }
 }
