0630c5d683e847124bb25b9e1788b594e7599717
Vyacheslav_Bakinskiy <vyacheslav.bakinskiy@life.com.by>
2021-03-16 11:07:31 +0300

    COF-1086 Some fixes

--- Depo/LifeBox_Business/Support/Extensions/String + Extension.swift
+++ Depo/LifeBox_Business/Support/Extensions/String + Extension.swift
@@ -41,7 +41,10 @@ extension String {
     }
     
     var attributedString: NSAttributedString? {
-        guard let data = data(using: .utf8) else { return nil }
+        guard let data = data(using: .utf8) else {
+            return nil
+        }
+        
         do {
             return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
         } catch {
