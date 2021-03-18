7fc2aaaff2450e6eb735712fc924ad63aefc512b
Vyacheslav_Bakinskiy <vyacheslav.bakinskiy@life.com.by>
2021-03-18 15:40:34 +0300

    COF-1141 Implemented UI for Contact Us vc

--- Depo/LifeBox_Business/Presentation/UserStories/Settings/ContactUsView/ContactUsViewController.swift
+++ Depo/LifeBox_Business/Presentation/UserStories/Settings/ContactUsView/ContactUsViewController.swift
@@ -23,27 +23,53 @@ final class ContactUsViewController: BaseViewController, NibInit {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
         setSubjectButton()
         setTextField()
         setSendButton()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //TODO: uncomment after merge with bar branch
+//        setNavigationBarStyle(.white)
+        
+//        if !Device.isIpad {
+//            defaultNavBarStyle()
+//        }
+    }
+    
     //MARK: - Setup
+    
+    private func setView() {
+        setTitle(withString: TextConstants.contactUsPageTitle)
+        //TODO: replace setTitle to it after merge with bar branch
+//        setNavigationTitle(title: TextConstants.contactUsPageTitle, isLargeTitle: false)
+        
+        descriptionLabel.font = UIFont.GTAmericaStandardRegularFont(size: 14)
+        descriptionLabel.textColor = ColorConstants.Text.textFieldText
+    }
     
     private func setSubjectButton() {
         subjectContainerView.layer.cornerRadius = 5
         subjectContainerView.layer.borderWidth = 1
         subjectContainerView.layer.borderColor = ColorConstants.a2FABorder.cgColor
+        
+        subjectLabel.text = TextConstants.contactUsSubjectBoxName
+        subjectLabel.font = UIFont.GTAmericaStandardRegularFont(size: 12)
+        subjectLabel.textColor = ColorConstants.Text.textFieldText
     }
     
     private func setTextField() {
         textField.layer.cornerRadius = 5
         textField.layer.borderWidth = 1
         textField.layer.borderColor = ColorConstants.a2FABorder.cgColor
+        
+        textField.placeholder = TextConstants.contactUsMessageBoxName
     }
     
     private func setSendButton() {
-//        sendButton.isEnabled = false
+        disableSendButton()
         
         sendButton.setTitleColor(UIColor.white, for: .normal)
         sendButton.titleLabel?.font = UIFont.GTAmericaStandardMediumFont(size: 14)
@@ -52,7 +78,15 @@ final class ContactUsViewController: BaseViewController, NibInit {
         sendButton.layer.cornerRadius = 5
     }
     
-
+    private func disableSendButton() {
+        sendButton.isEnabled = false
+        sendButton.alpha = 0.4
+    }
+    
+    private func enableSendButton() {
+        sendButton.isEnabled = true
+        sendButton.alpha = 1
+    }
     
     //MARK: - @IBActions
     
