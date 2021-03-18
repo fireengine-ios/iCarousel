f0541633bf98ce1e1d8c7b9d4ebcc3344a8da8ac
Vyacheslav_Bakinskiy <vyacheslav.bakinskiy@life.com.by>
2021-03-18 10:40:28 +0300

    COF-1141 Added Contact Us vc

---
+++ Depo/LifeBox_Business/Presentation/UserStories/Settings/ContactUsView/ContactUsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContactUsViewController.swift
+//  Depo
+//
+//  Created by Vyacheslav Bakinskiy on 18.03.21.
+//  Copyright © 2021 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+final class ContactUsViewController: ViewController {
+    
+}
