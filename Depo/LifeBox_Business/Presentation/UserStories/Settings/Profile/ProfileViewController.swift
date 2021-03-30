49a0bb60c1e9779fcca28162974f4cced35204df
Vyacheslav_Bakinskiy <vyacheslav.bakinskiy@life.com.by>
2021-03-30 14:40:50 +0300

    COF-1137 Set view appearence

--- Depo/LifeBox_Business/Presentation/UserStories/Settings/Profile/ProfileViewController.swift
+++ Depo/LifeBox_Business/Presentation/UserStories/Settings/Profile/ProfileViewController.swift
@@ -10,4 +10,36 @@ import UIKit
 
 final class ProfileViewController: BaseViewController, NibInit {
     
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changeLargeTitle(prefersLargeTitles: false, barStyle: .white)
+        setView()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setNavigationTitle(title: TextConstants.profilePageTitle, style: .white)
+        setNavigationBarStyle(.white)
+        
+        if !Device.isIpad {
+            setNavigationBarStyle(.byDefault)
+        }
+    }
+    
+    deinit {
+//        webView.navigationDelegate = nil
+//        webView.stopLoading()
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+    //MARK: - Private funcs
+    
+    private func setView() {
+        view.backgroundColor = ColorConstants.tableBackground
+    }
+    
+    
 }
