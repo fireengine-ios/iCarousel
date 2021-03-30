33d712e5d03b678bae5232d9cf080aa93c60da8d
Vyacheslav_Bakinskiy <vyacheslav.bakinskiy@life.com.by>
2021-03-30 13:37:59 +0300

    COF-1137 Added Profile vc

---
+++ Depo/LifeBox_Business/Presentation/UserStories/Settings/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Depo_LifeTech
+//
+//  Created by Vyacheslav Bakinskiy on 30.03.21.
+//  Copyright © 2021 LifeTech. All rights reserved.
+//
+
+import UIKit
+
+final class ProfileViewController: BaseViewController, NibInit {
+    
+}
