415f3309f4ab518bcf550301e003627026788aed
Anton Ignatovich <Anton.Ignatovich@life.com.by>
2021-04-02 14:21:25 +0300

    added shadows to menu item table view cell

--- Depo/LifeBox_Business/Presentation/UserStories/Settings/Settings/View/Cells/Item/SettingsMenuItemTableViewCell.swift
+++ Depo/LifeBox_Business/Presentation/UserStories/Settings/Settings/View/Cells/Item/SettingsMenuItemTableViewCell.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 final class SettingsMenuItemTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var innerContainerToTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var innerContainerToBottomConstraint: NSLayoutConstraint!
+
+    @IBOutlet private weak var innerContainerView: UIView! {
+        didSet {
+            innerContainerView.layer.cornerRadius = 5
+        }
+    }
+
     @IBOutlet private weak var iconImageView: UIImageView!
     @IBOutlet private weak var titleLabel: UILabel! {
         willSet {
@@ -31,8 +41,20 @@ final class SettingsMenuItemTableViewCell: UITableViewCell {
         layer.mask = nil
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        innerContainerView.layer.masksToBounds = false
+
+        innerContainerView.layer.shadowColor = UIColor.black.withAlphaComponent(0.25).cgColor
+        innerContainerView.layer.shadowOffset = CGSize.zero
+        innerContainerView.layer.shadowRadius = 5
+        innerContainerView.layer.shadowOpacity = 0.5
+    }
+
     func setup(with menuItem: SettingsMenuItem, isFirstCell: Bool = false, isLastCell: Bool = false) {
         let cornersToRound: UIRectCorner = isFirstCell ? [.topLeft, .topRight] : isLastCell ? [.bottomLeft, .bottomRight] : []
+        innerContainerToTopConstraint.constant = isFirstCell ? 2 : 0
+        innerContainerToBottomConstraint.constant = isLastCell ? 2 : 0
         roundCorners(corners: cornersToRound, radius: 5)
         iconImageView.image = menuItem.icon
         titleLabel.text = menuItem.title
