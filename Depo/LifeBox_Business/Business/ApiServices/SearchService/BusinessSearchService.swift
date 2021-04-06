01a9eebae648f14626763d537fb320993931f02b
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2021-04-06 10:07:16 +0300

    api

--- Depo/LifeBox_Business/Business/ApiServices/SearchService/BusinessSearchService.swift
+++ Depo/LifeBox_Business/Business/ApiServices/SearchService/BusinessSearchService.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2021 LifeTech. All rights reserved.
 //
 
+import Foundation
 import Alamofire
 import SwiftyJSON
 
@@ -27,45 +28,29 @@ class BusinessSearchService: BaseRequestService {
     
     private lazy var sessionManager: SessionManager = factory.resolve()
 
-//    Sample Request Body:
-//    { "diskTypes": [ "ME" ], "text": "string", "page": 0, "size": 20 }
-//    This API should be called with pagination
-//    "Text" field should contain what user typed in search area
-//    If you call this API on MY DISK page, set diskTypes as ME as above
-//    If you call this API on SHARED AREA page, set diskTypes as COMPANY
-    
-    
-    func storageUsageInfo(projectId: String,
-                          accoundId: String,
-                          handler: @escaping (ResponseResult<SettingsStorageUsageResponseItem>) -> Void) {
+///    Sample Request Body:
+///    { "diskTypes": [ "ME" ], "text": "string", "page": 0, "size": 20 }
+///    This API should be called with pagination
+///    "Text" field should contain what user typed in search area
+///    If you call this API on MY DISK page, set diskTypes as ME as above
+///    If you call this API on SHARED AREA page, set diskTypes as COMPANY
+    func search(text: String, diskType: SearchDiskTypes, page: Int, size: Int, handler: @escaping ResponseVoid) {
+        let url = RouteRequests.privateShareSearch
+        let params: [[String: Any]] = [[
+            "text": text,
+            "diskTypes": [diskType.requestProperty],
+            "page": page,
+            "size": size
+        ]]
+        let urlRequest: URLRequest? = sessionManager.request(url, method: .post).request
         
-
-        
-//        let url = RouteRequests.Account.Permissions.permissionsUpdate
-//        let params: [[String: Any]] = [["type": type.rawValue,
-//                                        "approved": isApproved]]
-//
-//        let urlRequest = sessionManager.request(url, method: .post).request
-//
-//        if var request = urlRequest {
-//            request = try! params.encode(request, with: nil)
-//
-//            sessionManager
-//                .request(request)
-//                .customValidate()
-//                .responseVoid(handler)
-//        }
-        
-        
-        guard let url = URL(string: String(format: RouteRequests.BusinessAccount.storageUsageInfo, projectId, accoundId)) else {
-            handler(.failed(ErrorResponse.string("Incorrect URL")))
-            return
+        if let unwrapedRequest = urlRequest,
+           let request = try? params.encode(unwrapedRequest, with: nil) {
+            sessionManager
+                .request(request)
+                .customValidate()
+                .responseVoid(handler)
         }
-
-        sessionManager
-            .request(url)
-            .customValidate()
-            .responseObject(handler)
     }
     
 }
