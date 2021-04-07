57f8a93379487f4de732984597ba9adacb237f45
Vyacheslav_Bakinskiy <vyacheslav.bakinskiy@life.com.by>
2021-04-07 12:12:19 +0300

    COF-1424 Changed preferred status bar style

--- Depo/LifeBox_Business/Presentation/UserStories/Others/PrivateShare/New Share/Views/Shared/PrivateShareSharedFilesViewController.swift
+++ Depo/LifeBox_Business/Presentation/UserStories/Others/PrivateShare/New Share/Views/Shared/PrivateShareSharedFilesViewController.swift
@@ -87,6 +87,10 @@ final class PrivateShareSharedFilesViewController: BaseViewController, Segmented
         collectionManager.reload(type: .onViewAppear)
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .default
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
