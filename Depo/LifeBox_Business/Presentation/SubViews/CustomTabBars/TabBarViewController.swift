d6d396aacfe9dd12e8dfde2c8e7f51d45318a1a9
Konstantin <>
2021-04-12 12:27:04 +0300

    do not layout animated

--- Depo/LifeBox_Business/Presentation/SubViews/CustomTabBars/TabBarViewController.swift
+++ Depo/LifeBox_Business/Presentation/SubViews/CustomTabBars/TabBarViewController.swift
@@ -196,10 +196,9 @@ final class TabBarViewController: ViewController, UITabBarDelegate {
                 self.musicBarHeightConstraint.constant = self.musicBar.isHidden ? 0 : self.musicBarH
                 self.setHeightConstantForUploadProgress()
                 debugLog("TabBarVC showTabBar about to layout")
-                self.view.layoutIfNeeded()
                 self.tabBar.isHidden = false
             }, completion: { _ in
-                
+                self.view.layoutIfNeeded()
             })
         }
     }
@@ -212,9 +211,9 @@ final class TabBarViewController: ViewController, UITabBarDelegate {
                 self.musicBarHeightConstraint.constant = 0
                 self.setHeightConstantForUploadProgress()
                 debugLog("TabBarVC showTabBar about to layout")
-                self.view.layoutIfNeeded()
             }, completion: { _ in
                 self.tabBar.isHidden = true
+                self.view.layoutIfNeeded()
             })
         }
     }
