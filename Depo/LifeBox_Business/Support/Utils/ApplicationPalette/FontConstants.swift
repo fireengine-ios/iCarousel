638c9c9c5e6e45dc04f9bc1e2a60ba99e406a175
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2021-04-14 10:43:35 +0300

    seconnd half

--- Depo/LifeBox_Business/Support/Utils/ApplicationPalette/FontConstants.swift
+++ Depo/LifeBox_Business/Support/Utils/ApplicationPalette/FontConstants.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2021 LifeTech. All rights reserved.
 //
 
-import Foundation
+extension UIFont {
+    
+    static func TurkcellSaturaDemFont(size: CGFloat = 9) -> UIFont {
+        return .systemFont(ofSize: size, weight: .semibold)//UIFont(name: "TurkcellSaturaDem", size: size)!
+    }
+    
+    static func TurkcellSaturaRegFont(size: CGFloat = 9) -> UIFont {
+        return .systemFont(ofSize: size, weight: .regular)//UIFont(name: "GTAmericaTrial-Rg", size: size)!
+    }
+    
+    static func TurkcellSaturaBolFont(size: CGFloat = 9) -> UIFont {
+        return .systemFont(ofSize: size, weight: .bold)//UIFont(name: "TurkcellSaturaBol", size: size)!
+    }
+    
+    static func TurkcellSaturaItaFont(size: CGFloat = 9) -> UIFont {
+        return .italicSystemFont(ofSize: size)//UIFont(name: "TurkcellSaturaIta", size: size)!
+    }
+    
+    static func TurkcellSaturaMedFont(size: CGFloat = 9) -> UIFont {
+        return .systemFont(ofSize: size, weight: .medium)//UIFont(name: "GTAmericaTrial-Md", size: size)!
+    }
+    
+    static func TurkcellSaturaFont(size: CGFloat = 18) -> UIFont {
+        return .systemFont(ofSize: size, weight: .regular)//UIFont(name: "GTAmericaTrial-Rg", size: size)!
+    }
+    
+    static func GTAmericaStandardRegularFont(size: CGFloat = 18) -> UIFont {
+        return .systemFont(ofSize: size, weight: .regular)//UIFont(name: "GTAmericaTrial-Rg", size: size)!
+    }
+    
+    static func GTAmericaStandardMediumFont(size: CGFloat = 18) -> UIFont {
+        return .systemFont(ofSize: size, weight: .medium)//UIFont(name: "GTAmericaTrial-Md", size: size)!
+    }
+}
+
