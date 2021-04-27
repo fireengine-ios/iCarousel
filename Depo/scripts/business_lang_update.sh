96eaa82b7132848745eefa0993b7936edadc13ea
EXT02D10729 <Aleksandr.Pestriakov@life.com.by>
2021-04-27 04:46:19 +0300

    added upload part

--- Depo/scripts/business_lang_update.sh
+++ Depo/scripts/business_lang_update.sh
@@ -14,24 +14,49 @@ function extractUrlFromPOEditorJson {
 function downloadOneLanguage {
     outputfile=$ROOT_LOCALIZATION_FOLDER$currentLangaugeCodeXCode".lproj/OurLocalizable.strings"
 
-    echo "*************** $currentLanguageCodePOEditor ->  $outputfile ***************"
+    echo "⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️ $currentLanguageCodePOEditor ->  $outputfile ⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️"
     echo Fetching POEditor URL...
 
 
     json=`curl -s -# -X POST https://poeditor.com/api/ \
-    -d api_token="$POEDITOR_API_TOKEN" \
     -d action="export" \
+    -d api_token="$POEDITOR_API_TOKEN" \
     -d id="$POEDITOR_PROJECT_ID" \
+    -d updating="terms_translations" \
     -d language="$currentLanguageCodePOEditor" \
     -d type="apple_strings"`
 
     url=`extractUrlFromPOEditorJson`
 
-    echo "***************"
+    echo "⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️"
     echo Downloading translation file...
 
     curl -# -s -X GET $url -o $outputfile
 }
+
+function uploadLanguage {
+    uploadFile=$ROOT_LOCALIZATION_FOLDER"en.lproj/OurLocalizable.strings"
+
+
+    echo "⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️ UPLOADING $uploadFile ⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️"
+    echo uploading...
+
+    uploadJson=`curl -X POST https://api.poeditor.com/v2/projects/upload \
+     -F api_token="$POEDITOR_API_TOKEN" \
+     -F id="$POEDITOR_PROJECT_ID" \
+     -F updating="terms_translations" \
+     -F file=@"$uploadFile" \
+     -F language="en" \
+     -F tags={"new":["name-of-tag"}` 
+
+    echo "$uploadJson"
+    echo "⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️"
+
+}
+
+echo " ▶️ Begining ▶️ "
+
+uploadLanguage
 
 for value in "${POEDITOR_LANGUAGES_VS_XCODE_FOLDERS[@]}"; do
     currentLanguageCodePOEditor="${value}"
