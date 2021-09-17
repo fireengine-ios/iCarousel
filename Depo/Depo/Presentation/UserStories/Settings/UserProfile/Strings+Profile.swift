11e888e2e0a38f9cbed7af6c70f24dee0f7e3e35
Hady Hallak <hadee.hallaq@consultant.turkcell.com.tr>
2021-09-17 08:36:51 +0300

    DE-11405 - Recovery Email
    Localize backend error RECOVERY_EMAIL_IS_SAME_WITH_ACCOUNT_EMAIL

--- Depo/Depo/Presentation/UserStories/Settings/UserProfile/Strings+Profile.swift
+++ Depo/Depo/Presentation/UserStories/Settings/UserProfile/Strings+Profile.swift
@@ -10,13 +10,14 @@ import Foundation
 
 extension Strings {
     enum Profile: String, Localizable {
-        case profileRecoveryMail            = "profile_recovery_mail"
-        case profileRecoveryMailDescription = "profile_recovery_mail_desc"
-        case profileRecoveryMailHint        = "profile_recovery_mail_hint"
-        case profileMailVerified            = "profile_mail_verified"
-        case profileVerifyButtonTitle       = "profile_verify"
-        case profileRecoveryEmailIsEmpty    = "profile_recovery_email_empty"
-        case profileRecoveryEmailIsInvalid  = "profile_recovery_email_invalid"
+        case profileRecoveryMail                        = "profile_recovery_mail"
+        case profileRecoveryMailDescription             = "profile_recovery_mail_desc"
+        case profileRecoveryMailHint                    = "profile_recovery_mail_hint"
+        case profileMailVerified                        = "profile_mail_verified"
+        case profileVerifyButtonTitle                   = "profile_verify"
+        case profileRecoveryEmailIsEmpty                = "profile_recovery_email_empty"
+        case profileRecoveryEmailIsInvalid              = "profile_recovery_email_invalid"
+        case profileRecoveryEmailIsSameWithAccountEmail = "profile_recovery_email_is_same_with_account_email"
     }
 }
 
