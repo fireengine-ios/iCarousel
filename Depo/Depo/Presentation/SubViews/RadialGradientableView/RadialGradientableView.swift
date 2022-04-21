4024c1876daf59f48d6eae220abfb88b301dc016
Hady Hallak <hadee.hallaq@consultant.turkcell.com.tr>
2022-04-21 15:53:23 +0300

    temp fix for debug hierarchy

--- Depo/Depo/Presentation/SubViews/RadialGradientableView/RadialGradientableView.swift
+++ Depo/Depo/Presentation/SubViews/RadialGradientableView/RadialGradientableView.swift
@@ -9,27 +9,29 @@
 import UIKit
 
 final class RadialGradientableView: UIView {
-    
-    var gradientColors: [CGColor] {
-        get { return viewLayer.gradientColors }
-        set { viewLayer.gradientColors = newValue }
-    }
-    
-    lazy var viewLayer: RadialGradientableLayer = {
-        if let layer = layer as? RadialGradientableLayer {
-            return layer
-        } else {
-            assertionFailure("in 'class var layerClass' must be RadialGradientableLayer")
-            return RadialGradientableLayer()
-        }
-    }()
-    
-    var isNeedGradient: Bool {
-        get { return viewLayer.isNeedGradient }
-        set { viewLayer.isNeedGradient = newValue }
-    }
-    
-    override class var layerClass: Swift.AnyClass {
-        return RadialGradientableLayer.self
-    }
+
+    // TODO: Facelift. RadialGradientableLayer is breaking debug view hierarchy
+    var isNeedGradient = false
+//    var gradientColors: [CGColor] {
+//        get { return viewLayer.gradientColors }
+//        set { viewLayer.gradientColors = newValue }
+//    }
+//
+//    lazy var viewLayer: RadialGradientableLayer = {
+//        if let layer = layer as? RadialGradientableLayer {
+//            return layer
+//        } else {
+//            assertionFailure("in 'class var layerClass' must be RadialGradientableLayer")
+//            return RadialGradientableLayer()
+//        }
+//    }()
+//
+//    var isNeedGradient: Bool {
+//        get { return viewLayer.isNeedGradient }
+//        set { viewLayer.isNeedGradient = newValue }
+//    }
+//
+//    override class var layerClass: Swift.AnyClass {
+//        return RadialGradientableLayer.self
+//    }
 }
