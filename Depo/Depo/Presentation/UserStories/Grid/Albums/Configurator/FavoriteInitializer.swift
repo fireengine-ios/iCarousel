4d6b371af5a9629dcbb3fdcfef0888d1623fb5b5
Ozan Salman <ozan.salman@consultant.turkcell.com.tr>
2023-01-16 09:15:02 +0300

    LB-2827 Foryou favorite section

---
+++ Depo/Depo/Presentation/UserStories/Grid/Albums/Configurator/FavoriteInitializer.swift
@@ -0,0 +1,9 @@
+//
+//  FavoriteInitializer.swift
+//  Depo
+//
+//  Created by Ozan Salman on 11.01.2023.
+//  Copyright © 2023 LifeTech. All rights reserved.
+//
+
+import Foundation
---
+++ Depo/Depo/Presentation/UserStories/Grid/Albums/Interactor/FavoriteInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  FavoriteInteractor.swift
+//  Depo
+//
+//  Created by Ozan Salman on 12.01.2023.
+//  Copyright © 2023 LifeTech. All rights reserved.
+//
+
+import Foundation
