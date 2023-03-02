5292c948afecd095676677660cab03cb24c0cc71
Ozan Salman <ozan.salman@consultant.turkcell.com.tr>
2023-03-02 19:11:38 +0300

    LB-639 create collage configurator created

--- Depo/Depo/Presentation/SubViews/CreateCollage/Configurator/CreateCollageInitilizer.swift
+++ Depo/Depo/Presentation/SubViews/CreateCollage/Configurator/CreateCollageInitilizer.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class CreateCollageInitilizer: NSObject {
+//    class func initializeViewController(with nibName: String) -> HeaderContainingViewController.ChildViewController {
+//        let viewController = CreateCollageViewController(nibName: nibName, bundle: nil)
+//        let configurator = CreateCollageConfigurator()
+//        configurator.configureModuleForViewInput(viewInput: viewController)
+//        viewController.floatingButtonsArray.append(contentsOf: [.takePhoto, .upload, .createAStory, .createAlbum, .photopick])
+//        return viewController
+//    }
+    class func initializeViewController() -> CreateCollageViewController {
+        let viewController = CreateCollageViewController()
+        let configurator = CreateCollageConfigurator()
+        configurator.configure(viewController: viewController)
+        return viewController
+    }
+}
