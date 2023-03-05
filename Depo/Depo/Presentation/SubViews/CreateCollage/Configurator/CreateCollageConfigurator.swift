8fdcc11e3257e07413532e214bd0443146dde265
Ozan Salman <ozan.salman@consultant.turkcell.com.tr>
2023-03-05 17:04:51 +0300

    LB-639 create collage configurator and initilizer code refactor

--- Depo/Depo/Presentation/SubViews/CreateCollage/Configurator/CreateCollageConfigurator.swift
+++ Depo/Depo/Presentation/SubViews/CreateCollage/Configurator/CreateCollageConfigurator.swift
@@ -9,26 +9,7 @@
 import Foundation
 
 class CreateCollageConfigurator {
-//    func configureModuleForViewInput<UIViewController>(viewInput: UIViewController) {
-//        if let viewController = viewInput as? CreateCollageViewController {
-//            configure(viewController: viewController)
-//        }
-//    }
-//
-//    private func configure(viewController: CreateCollageViewController) {
-//        let router = CreateCollageRouter()
-//
-//        let presenter = CreateCollagePresenter()
-//        presenter.view = viewController
-//        presenter.router = router
-//        router.presenter = presenter
-//
-//        let interactor = CreateCollageInteractor()
-//        interactor.output = presenter
-//
-//        presenter.interactor = interactor
-//        viewController.output = presenter
-//    }
+    
     func configure(viewController: CreateCollageViewController) {
 
         let router = CreateCollageRouter()
--- Depo/Depo/Presentation/SubViews/CreateCollage/Configurator/CreateCollageInitilizer.swift
+++ Depo/Depo/Presentation/SubViews/CreateCollage/Configurator/CreateCollageInitilizer.swift
@@ -9,13 +9,7 @@
 import Foundation
 
 class CreateCollageInitilizer: NSObject {
-//    class func initializeViewController(with nibName: String) -> HeaderContainingViewController.ChildViewController {
-//        let viewController = CreateCollageViewController(nibName: nibName, bundle: nil)
-//        let configurator = CreateCollageConfigurator()
-//        configurator.configureModuleForViewInput(viewInput: viewController)
-//        viewController.floatingButtonsArray.append(contentsOf: [.takePhoto, .upload, .createAStory, .createAlbum, .photopick])
-//        return viewController
-//    }
+    
     class func initializeViewController() -> CreateCollageViewController {
         let viewController = CreateCollageViewController()
         let configurator = CreateCollageConfigurator()
