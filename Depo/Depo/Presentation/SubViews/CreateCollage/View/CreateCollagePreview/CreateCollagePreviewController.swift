92312975ce71d1f00b05e16af4fb9599c9052a87
Ozan Salman <ozan.salman@consultant.turkcell.com.tr>
2023-03-10 10:03:49 +0300

    LB-3482 preview controller

--- Depo/Depo/Presentation/SubViews/CreateCollage/View/CreateCollagePreview/CreateCollagePreviewController.swift
+++ Depo/Depo/Presentation/SubViews/CreateCollage/View/CreateCollagePreview/CreateCollagePreviewController.swift
@@ -7,8 +7,11 @@
 //
 
 import Foundation
+import SDWebImage
 
 final class CreateCollagePreviewController: BaseViewController {
+    
+    let router = RouterVC()
     
     private lazy var containerView: UIView = {
         let view = UIView()
@@ -25,13 +28,15 @@ final class CreateCollagePreviewController: BaseViewController {
         return view
     }()
     
+    private var longPressedItem: Int = -1
     private var collagePhotoCount: Int = 0
-    private var selectedItems = [SearchItemResponse]()
-    private var collageTemplate: CollageTemplateElement?
     private var xRatioConstant: Double = 0
     private var yRatioConstant: Double = 0
     private var xFrameStart: Double = 30
     private var yFrameStart: Double = 100
+    private var selectedItems = [SearchItemResponse]()
+    private var collageTemplate: CollageTemplateElement?
+    private lazy var bottomBarManager = CreateCollageBottomBarManager(delegate: self)
     
     init(collageTemplate: CollageTemplateElement, selectedItems: [SearchItemResponse]) {
         self.collageTemplate = collageTemplate
@@ -48,16 +53,24 @@ final class CreateCollagePreviewController: BaseViewController {
         super.viewDidLoad()
         debugLog("CreateCollagePreviewController viewDidLoad")
         
+        bottomBarManager.setup()
         setLayout()
         createImageView(collageTemplate: collageTemplate!, imageCount: selectedItems.count)
         view.backgroundColor = .white
         setTitle(withString: "Create Collage")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        bottomBarManager.editingTabBar?.view.layoutIfNeeded()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        bottomBarManager.hide()
+    }
+    
     private func createImageView(collageTemplate: CollageTemplateElement, imageCount: Int) {
         let shapeDetails = collageTemplate.shapeDetails.sorted { $0.id < $1.id }
-        
-        
 
         for i in 0...shapeDetails.count - 1 {
             if shapeDetails[i].type == "RECTANGLE" {
@@ -73,16 +86,56 @@ final class CreateCollagePreviewController: BaseViewController {
 
                 let viewFrame = CGRect(x: xStart, y: yStart, width: imageWidth / xRatioConstant, height: imageHeight / xRatioConstant)
                 let imageView = UIImageView(frame: viewFrame)
-                imageView.layer.cornerRadius = 4
-                imageView.layer.masksToBounds = false
-                imageView.backgroundColor = .red
+                
+                imageView.tag = i
+                imageView.isUserInteractionEnabled = true
+                
+                if selectedItems.count == 0 {
+                    let tapImage = UITapGestureRecognizer(target: self, action: #selector(thumbnailImageTapped(_:)))
+                    imageView.addGestureRecognizer(tapImage)
+                    imageView.backgroundColor = AppColor.collageThumbnailColor.color
+                    imageView.contentMode = .center
+                    imageView.image = Image.iconAddUnselect.image
+                } else {
+                    let longTapImage = UILongPressGestureRecognizer(target: self, action: #selector(actionImage(_:)))
+                    imageView.addGestureRecognizer(longTapImage)
+                    imageView.contentMode = .scaleToFill
+                    let imageUrl = selectedItems[i].metadata?.mediumUrl
+                    imageView.sd_setImage(with: imageUrl)
+                }
+                
                 contentView.addSubview(imageView)
-                    
             }
         }
     }
+    
+    @objc func thumbnailImageTapped(_ sender:AnyObject) {
+        let vc = router.createCollageSelectPhotos(collageTemplate: collageTemplate!)
+        router.pushViewController(viewController: vc, animated: false)
+    }
+    
+    @objc func actionImage(_ sender: UIGestureRecognizer) {
+        if sender.state == .began {
+            let shapeDetails = collageTemplate?.shapeDetails.sorted { $0.id < $1.id }
+            if sender.view?.tag == longPressedItem {
+                for i in 0...(shapeDetails?.count ?? 1) - 1 {
+                    contentView.subviews[i].alpha = 1
+                }
+            } else {
+                for i in 0...(shapeDetails?.count ?? 1) - 1 {
+                    if sender.view?.tag != i {
+                        contentView.subviews[i].alpha = 0.5
+                    } else {
+                        longPressedItem = sender.view?.tag ?? -1
+                        contentView.subviews[i].alpha = 1
+                    }
+                }
+            }
+        }
+    }
+    
+    
 }
-
 
 extension CreateCollagePreviewController {
     private func setLayout() {
@@ -96,9 +149,63 @@ extension CreateCollagePreviewController {
         let rightSpace = xFrameStart * 2
         
         contentView.frame = CGRect(x: xFrameStart, y: yFrameStart, width: viewWidth - rightSpace, height: (viewWidth - rightSpace) / yRatio)
-        
         xRatioConstant = Double(collageTemplate?.imageWidth ?? 1) / Double(contentView.frame.size.width)
-        
         containerView.frame = CGRect(x: contentView.frame.origin.x - 6, y: contentView.frame.origin.y - 6, width: contentView.frame.size.width + 12, height: contentView.frame.size.height + 12)
+        
+        if selectedItems.count > 0 {
+            bottomBarManager.show()
+        }
+        
+    }
+}
+
+extension CreateCollagePreviewController: BaseItemInputPassingProtocol {
+    func operationFinished(withType type: ElementTypes, response: Any?) {
+        
+    }
+    
+    func operationFailed(withType type: ElementTypes) {
+        
+    }
+    
+    func stopModeSelected() {
+    }
+    
+    func delete(all: Bool) {
+        print("aaaaaaaaa")
+    }
+    
+    func selectModeSelected() {
+    }
+    
+    func selectAllModeSelected() {
+    }
+    
+    func showOnly(withType type: ElementTypes) {
+        
+    }
+    
+    func deSelectAll() {
+        
+    }
+    
+    func printSelected() {
+        
+    }
+    
+    func changeCover() {
+        print("CHANGE")
+    }
+    
+    func changePeopleThumbnail() {
+        
+    }
+    
+    func openInstaPick() {
+    
+    }
+    
+    func getSelectedItems(selectedItemsCallback: @escaping BaseDataSourceItems) {
+        selectedItemsCallback([])
     }
 }
