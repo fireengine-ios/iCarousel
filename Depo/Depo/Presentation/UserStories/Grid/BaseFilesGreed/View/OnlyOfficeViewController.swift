0a12cc19f372277fe0dedf4ac3b57ce23e4e657a
Ozan Salman <ozan.salman@consultant.turkcell.com.tr>
2023-04-06 10:32:51 +0300

    LB-3368 OnlyOfficeViewController added

---
+++ Depo/Depo/Presentation/UserStories/Grid/BaseFilesGreed/View/OnlyOfficeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OnlyOfficeViewController.swift
+//  Depo
+//
+//  Created by Ozan Salman on 6.04.2023.
+//  Copyright © 2023 LifeTech. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+class OnlyOfficeViewController: BaseViewController {
+    
+    @IBOutlet weak var onlyOfficeWebView: WKWebView!
+    
+}
