0feba25ff541c68f6471bc6c88cc879b2f28e4f6
TURKCELL\EXT02D51886 <ozan.salman@consultant.turkcell.com.tr>
2023-07-20 11:50:47 +0300

    Develop branch set before facelift create branch

--- Depo/Depo/App/AppDelegate.swift
+++ Depo/Depo/App/AppDelegate.swift
@@ -572,7 +572,7 @@ extension AppDelegate {
                     }
 
                     
-                    if userActivity.userInfo?.count != 0 && PushNotificationService.shared.assignDeepLink(innerLink: host, options: userActivity.userInfo) {
+                    if PushNotificationService.shared.assignDeepLink(innerLink: host, options: userActivity.userInfo) {
                         debugLog("Should open Action Screen")
                         PushNotificationService.shared.openActionScreen()
                     }
