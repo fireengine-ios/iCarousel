9347c3bcf084d71d5714fc6d638eebefb107f5d1
Yahya Çağrı Öztürk <ozturk.yahya@consultant.turkcell.com.tr>
2023-09-25 16:32:32 +0300

    toast message fix

--- Depo/Depo/Presentation/UserStories/Others/MoreFilesActions/Interactor/MoreFilesActionsInteractor.swift
+++ Depo/Depo/Presentation/UserStories/Others/MoreFilesActions/Interactor/MoreFilesActionsInteractor.swift
@@ -678,7 +678,7 @@ class MoreFilesActionsInteractor: NSObject, MoreFilesActionsInteractorInput {
         }
         
         let okHandler: PopUpButtonHandler = { [weak self] vc in
-            SnackbarManager.shared.show(type: .nonCritical, message: "Talebiniz alınmıştır")
+            SnackbarManager.shared.show(type: .nonCritical, message: TextConstants.toastMessageDeleteNotification)
             self?.analyticsService.trackFileOperationPopupGAEvent(operationType: .unhide, label: .ok)
             self?.output?.operationStarted(type: .unhide)
             vc.close { [weak self] in
@@ -714,7 +714,7 @@ class MoreFilesActionsInteractor: NSObject, MoreFilesActionsInteractorInput {
         }
 
         let okHandler: PopUpButtonHandler = { [weak self] vc in
-            SnackbarManager.shared.show(type: .nonCritical, message: "Talebiniz alınmıştır")
+            SnackbarManager.shared.show(type: .nonCritical, message: TextConstants.toastMessageDeleteNotification)
             self?.analyticsService.trackFileOperationPopupGAEvent(operationType: .restore, label: .ok)
             self?.output?.operationStarted(type: .restore)
             vc.close { [weak self] in
@@ -1729,13 +1729,13 @@ extension MoreFilesActionsInteractor {
     }
     
     private func unhideSelectedItems(_ items: [Item], success: @escaping FileOperation, fail: @escaping ((Error) -> Void)) {
-        SnackbarManager.shared.show(type: .nonCritical, message: "Talebiniz alınmıştır")
+        SnackbarManager.shared.show(type: .nonCritical, message: TextConstants.toastMessageDeleteNotification)
         analyticsService.trackFileOperationGAEvent(operationType: .unhide, items: items)
         fileService.unhide(items: items, success: success, fail: fail)
     }
     
     private func unhideAlbums(_ items: [AlbumItem], success: @escaping FileOperation, fail: @escaping ((Error) -> Void)) {
-        SnackbarManager.shared.show(type: .nonCritical, message: "Talebiniz alınmıştır")
+        SnackbarManager.shared.show(type: .nonCritical, message: TextConstants.toastMessageDeleteNotification)
         analyticsService.trackAlbumOperationGAEvent(operationType: .unhide, albums: items)
         fileService.unhideAlbums(items, success: success, fail: fail)
     }
@@ -1920,7 +1920,7 @@ extension MoreFilesActionsInteractor {
     }
     
     private func putBackSelectedItems(_ items: [Item], success: @escaping FileOperation, fail: @escaping ((Error) -> Void)) {
-        SnackbarManager.shared.show(type: .nonCritical, message: "Talebiniz alınmıştır")
+        SnackbarManager.shared.show(type: .nonCritical, message: TextConstants.toastMessageDeleteNotification)
         analyticsService.trackFileOperationGAEvent(operationType: .restore, items: items)
         fileService.putBack(items: items, success: { [weak self] in
             self?.removeItemsFromPlayer(items: items)
@@ -1929,13 +1929,13 @@ extension MoreFilesActionsInteractor {
     }
     
     private func putBackAlbums(_ items: [AlbumItem], success: @escaping FileOperation, fail: @escaping ((Error) -> Void)) {
-        SnackbarManager.shared.show(type: .nonCritical, message: "Talebiniz alınmıştır")
+        SnackbarManager.shared.show(type: .nonCritical, message: TextConstants.toastMessageDeleteNotification)
         analyticsService.trackAlbumOperationGAEvent(operationType: .restore, albums: items)
         fileService.putBackAlbums(items, success: success, fail: fail)
     }
     
     private func putBackFIRAlbums(_ items: [Item], success: @escaping FileOperation, fail: @escaping ((Error) -> Void)) {
-        SnackbarManager.shared.show(type: .nonCritical, message: "Talebiniz alınmıştır")
+        SnackbarManager.shared.show(type: .nonCritical, message: TextConstants.toastMessageDeleteNotification)
         if let items = items as? [PeopleItem] {
             analyticsService.trackFileOperationGAEvent(operationType: .restore, itemsType: .people, itemsCount: items.count)
             fileService.putBackPeople(items: items, success: success, fail: fail)
