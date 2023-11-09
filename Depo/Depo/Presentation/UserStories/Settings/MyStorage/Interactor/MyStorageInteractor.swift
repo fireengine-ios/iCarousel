c47ed1e46844713b513b4697b582385631b19201
TURKCELL\EXT02D51886 <ozan.salman@consultant.turkcell.com.tr>
2023-11-09 13:04:57 +0300

    LB-5673 packages screen

--- Depo/Depo/Presentation/UserStories/Settings/MyStorage/Interactor/MyStorageInteractor.swift
+++ Depo/Depo/Presentation/UserStories/Settings/MyStorage/Interactor/MyStorageInteractor.swift
@@ -288,7 +288,7 @@ extension MyStorageInteractor: MyStorageInteractorInput {
                 SingletonStorage.shared.activeUserSubscription = subscriptionsResponse
                 let offersList = subscriptionsResponse.list
                 
-                self?.output.successed(allOffers: offersList)
+                //self?.output.successed(allOffers: offersList)
                 self?.getInfoForAppleProducts(offers: offersList)
             }, fail: { [weak self] errorResponse in
                 DispatchQueue.toMain {
--- Depo/Depo/Presentation/UserStories/Settings/MyStorage/Presenter/MyStoragePresenter.swift
+++ Depo/Depo/Presentation/UserStories/Settings/MyStorage/Presenter/MyStoragePresenter.swift
@@ -61,7 +61,7 @@ final class MyStoragePresenter {
         
         startActivity()
         interactor.getAllOffers()
-        interactor.getAvailableOffers(with: accountType)
+        //interactor.getAvailableOffers(with: accountType)
     }
     
     //MARK: - UtilityMethods
@@ -71,7 +71,7 @@ final class MyStoragePresenter {
         if let index = displayableOffers.firstIndex(where: { $0.type == .free }) {
             displayableOffers.swapAt(0, index)
         }
-        
+        interactor.getAvailableOffers(with: accountType)
         stopActivity()
         view?.reloadPackages()
     }
@@ -266,7 +266,7 @@ extension MyStoragePresenter: MyStorageInteractorOutput {
     func successedPackages(accountTypeString: String) {
         accountType = interactor.getAccountTypePackages(with: accountTypeString, offers: []) ?? .all
         view?.showInAppPolicy()
-        interactor.getAvailableOffers(with: accountType)
+        //interactor.getAvailableOffers(with: accountType)
     }
     
     func failedVerifyOffer() {
