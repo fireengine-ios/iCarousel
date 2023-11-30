ca4d0bb48df200a72797d3882cd188efdc9dd171
TURKCELL\EXT02D51886 <ozan.salman@consultant.turkcell.com.tr>
2023-11-30 14:53:38 +0300

    LB-5677 netmera sdk update and update notification service extension file

---
+++ Depo/LifeboxNotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,22 @@
+//
+//  NotificationService.swift
+//  LifeboxNotificationServiceExtension
+//
+//  Created by Ozan Salman on 30.11.2023.
+//  Copyright © 2023 LifeTech. All rights reserved.
+//
+
+import UserNotifications
+import NetmeraNotificationServiceExtension
+
+class NotificationService : NetmeraNotificationServiceExtension {
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (_ contentToDeliver: UNNotificationContent) -> Void) {
+        super.didReceive(request, withContentHandler: contentHandler)
+    }
+
+    override func serviceExtensionTimeWillExpire() {
+        super.serviceExtensionTimeWillExpire()
+    }
+
+}
