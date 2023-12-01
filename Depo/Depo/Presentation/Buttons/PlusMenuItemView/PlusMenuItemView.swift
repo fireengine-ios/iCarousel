b217449581580030c6c8cb8a47a277291a6cbee8
Yahya Çağrı Öztürk <ozturk.yahya@consultant.turkcell.com.tr>
2023-12-01 09:36:26 +0300

    icon fix

--- Depo/Depo/Presentation/Buttons/PlusMenuItemView/PlusMenuItemView.swift
+++ Depo/Depo/Presentation/Buttons/PlusMenuItemView/PlusMenuItemView.swift
@@ -87,7 +87,7 @@ enum FloatingButtonsType {
         case .photopick:
             return Image.iconEffect.image
         case .createCollage:
-            return Image.iconEffect.image
+            return Image.iconCollage.image
         case .createWord:
             return Image.iconFileDocNew.image
         case .createExcel:
