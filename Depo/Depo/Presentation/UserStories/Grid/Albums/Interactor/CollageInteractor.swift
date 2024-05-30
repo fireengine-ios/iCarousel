b75808dc9698f84184b3d65eac0270d3125419ae
TURKCELL\EXT02D055870 <rustem.manafli@consultant.turkcell.com.tr>
2024-05-30 15:26:17 +0400

    fixed captcha issue and collage sorting

--- Depo/Depo/Presentation/UserStories/Grid/Albums/Interactor/CollageInteractor.swift
+++ Depo/Depo/Presentation/UserStories/Grid/Albums/Interactor/CollageInteractor.swift
@@ -15,16 +15,49 @@ class CollageInteractor: BaseFilesGreedInteractor {
     override func getAllItems(sortBy: SortedRules) {
         debugLog("CollageInteractor getAllItems")
         
-        getCollages()
+        getCollages(sortBy: sortBy)
     }
     
-    private func getCollages() {
+    private func getCollages(sortBy: SortedRules) {
         debugLog("ForYou getCollages")
         service.forYouCollages() { [weak self] result in
             switch result {
             case .success(let response):
                 var array = [[BaseDataSourceItem]]()
-                array.append(response.fileList)
+                var sortedList: [BaseDataSourceItem] = response.fileList
+                
+                switch sortBy {
+                case .lettersAZ:
+                    sortedList.sort { (item1: BaseDataSourceItem, item2: BaseDataSourceItem) -> Bool in
+                        (item1.name ?? "") < (item2.name ?? "")
+                    }
+                case .lettersZA:
+                    sortedList.sort { (item1: BaseDataSourceItem, item2: BaseDataSourceItem) -> Bool in
+                        (item1.name ?? "") > (item2.name ?? "")
+                    }
+                case .timeUp:
+                    sortedList.sort { (item1: BaseDataSourceItem, item2: BaseDataSourceItem) -> Bool in
+                        (item1.creationDate ?? Date.distantPast) < (item2.creationDate ?? Date.distantPast)
+                    }
+                case .timeDown:
+                    sortedList.sort { (item1: BaseDataSourceItem, item2: BaseDataSourceItem) -> Bool in
+                        (item1.creationDate ?? Date.distantPast) > (item2.creationDate ?? Date.distantPast)
+                    }
+                case .sizeAZ:
+//                    sortedList.sort { (item1: BaseDataSourceItem, item2: BaseDataSourceItem) -> Bool in
+//                        (item1.size ?? 0) < (item2.size ?? 0)
+//                    }
+                    print("")
+                case .sizeZA:
+//                    sortedList.sort { (item1: BaseDataSourceItem, item2: BaseDataSourceItem) -> Bool in
+//                        (item1.size ?? 0) > (item2.size ?? 0)
+//                    }
+                    print("")
+                case .albumlettersAZ, .albumlettersZA, .metaDataTimeUp, .metaDataTimeDown, .lastModifiedTimeUp, .lastModifiedTimeDown, .timeUpWithoutSection, .timeDownWithoutSection:
+                    break
+                }
+                
+                array.append(sortedList)
                 self?.output.getContentWithSuccess(array: array)
             case .failed:
                 break
