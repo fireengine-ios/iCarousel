dce34bd3b3fff47a2a5fe6a56a4df2c0c680098a
TURKCELL\EXT02D51886 <ozan.salman@consultant.turkcell.com.tr>
2024-06-24 13:01:49 +0300

    LB-7470 biOgrenci url link get from api

--- Depo/Depo/Presentation/SubViews/ViewForPopUp/PopUpSubViews/InvitationCard/InvitationCard.swift
+++ Depo/Depo/Presentation/SubViews/ViewForPopUp/PopUpSubViews/InvitationCard/InvitationCard.swift
@@ -9,128 +9,132 @@ import Foundation
 
 final class InvitationCard: BaseCardView {
     
-  @IBOutlet weak var containerStackView: UIStackView!
-  @IBOutlet weak var playButton: UIButton!
-  @IBOutlet weak var invitationImageView: LoadingImageView!
-  @IBOutlet weak var bottomButton: UIButton!
-  private lazy var analyticsService: AnalyticsService = factory.resolve()
-  private var videoUrl: URL?
-  private var viewType: OperationType = .invitation
+    @IBOutlet weak var containerStackView: UIStackView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var invitationImageView: LoadingImageView!
+    @IBOutlet weak var bottomButton: UIButton!
+    private lazy var analyticsService: AnalyticsService = factory.resolve()
+    private var videoUrl: URL?
+    private var viewType: OperationType = .invitation
+    private var detailsUrl: String = ""
     
-  func configurateWithType(viewType: OperationType) {
-    self.viewType = viewType
-    let title = viewType == .invitation ? TextConstants.homeInvitationCardButtn : localized(.paycellCampaignTitle)
-    bottomButton.setTitle(title, for: .normal)
-    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
-    invitationImageView.isUserInteractionEnabled = true
-    invitationImageView.addGestureRecognizer(tapGestureRecognizer)
-  }
+    func configurateWithType(viewType: OperationType) {
+        self.viewType = viewType
+        let title = viewType == .invitation ? TextConstants.homeInvitationCardButtn : localized(.paycellCampaignTitle)
+        bottomButton.setTitle(title, for: .normal)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        invitationImageView.isUserInteractionEnabled = true
+        invitationImageView.addGestureRecognizer(tapGestureRecognizer)
+    }
     
-  @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
-  {
-    let router = RouterVC()
-    if viewType == .invitation {
-      analyticsService.trackCustomGAEvent(eventCategory: .functions, eventActions: .invitation, eventLabel: .homePageCard(.letsSee))
-      AnalyticsService.sendNetmeraEvent(event: NetmeraEvents.Actions.HomepageCard(cardName: NetmeraEventValues.HomePageCardEventValue.invitation.text, action: NetmeraEventValues.HomePageCardEventValue.detail.text))
-      let controller = router.invitationController()
-      router.pushViewController(viewController: controller)
-    } else if viewType == .paycell {
-      router.pushViewController(viewController: router.paycell)
-    } else if viewType == .drawCampaign {
-      let url = URL(string: RouteRequests.drawCampaignUrl)
-      UIApplication.shared.open(url!)
-    } else if viewType == .milliPiyango {
-      let vc = TermsDescriptionController(text: "")
-      router.pushViewController(viewController: vc)
-    } else if viewType == .biOgrenci {
-      let url = URL(string: RouteRequests.biOgrenciUrl)
-      UIApplication.shared.open(url!)
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let router = RouterVC()
+        if viewType == .invitation {
+            analyticsService.trackCustomGAEvent(eventCategory: .functions, eventActions: .invitation, eventLabel: .homePageCard(.letsSee))
+            AnalyticsService.sendNetmeraEvent(event: NetmeraEvents.Actions.HomepageCard(cardName: NetmeraEventValues.HomePageCardEventValue.invitation.text, action: NetmeraEventValues.HomePageCardEventValue.detail.text))
+            let controller = router.invitationController()
+            router.pushViewController(viewController: controller)
+        } else if viewType == .paycell {
+            router.pushViewController(viewController: router.paycell)
+        } else if viewType == .drawCampaign {
+            let url = URL(string: RouteRequests.drawCampaignUrl)
+            UIApplication.shared.open(url!)
+        } else if viewType == .milliPiyango {
+            let vc = TermsDescriptionController(text: "")
+            router.pushViewController(viewController: vc)
+        } else if viewType == .biOgrenci {
+            let url = URL(string: detailsUrl)
+            UIApplication.shared.open(url!)
+        }
     }
-  }
     
-  override func configurateView() {
-    super.configurateView()
-    bottomButton.setTitleColor(UIColor.lrTealish, for: .normal)
-    bottomButton.titleLabel?.font = UIFont.TurkcellSaturaBolFont(size: 14)
-    bottomButton.setTitleColor(ColorConstants.blueColor, for: .normal)
-    bottomButton.adjustsFontSizeToFitWidth()
-  }
+    override func configurateView() {
+        super.configurateView()
+        bottomButton.setTitleColor(UIColor.lrTealish, for: .normal)
+        bottomButton.titleLabel?.font = UIFont.TurkcellSaturaBolFont(size: 14)
+        bottomButton.setTitleColor(ColorConstants.blueColor, for: .normal)
+        bottomButton.adjustsFontSizeToFitWidth()
+    }
     
-  override func layoutSubviews() {
-    super.layoutSubviews()
-    //let height = containerStackView.frame.size.height DESIGN WITH BUTTON
-    let height = invitationImageView.frame.size.height //DESIGN WITHOUT BUTTON
-    if calculatedH != height {
-      calculatedH = height
-      layoutIfNeeded()
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //let height = containerStackView.frame.size.height DESIGN WITH BUTTON
+        let height = invitationImageView.frame.size.height //DESIGN WITHOUT BUTTON
+        if calculatedH != height {
+            calculatedH = height
+            layoutIfNeeded()
+        }
     }
-  }
     
-  override func set(object: HomeCardResponse?) {
-    super.set(object: object)
-    var thumbnailUrl: String = ""
-    switch viewType {
-    case .biOgrenci:
-      thumbnailUrl = "imageUrl"
-    default:
-      thumbnailUrl = "thumbnail"
+    override func set(object: HomeCardResponse?) {
+        super.set(object: object)
+        var thumbnailUrl: String = ""
+        switch viewType {
+        case .biOgrenci:
+            thumbnailUrl = "imageUrl"
+            if let url = cardObject?.details?["detailsUrl"].string {
+                detailsUrl = url
+            }
+        default:
+            thumbnailUrl = "thumbnail"
+        }
+        configurateByResponseObject(thumbnailUrl: thumbnailUrl)
     }
-    configurateByResponseObject(thumbnailUrl: thumbnailUrl)
-  }
     
-  private func configurateByResponseObject(thumbnailUrl: String) {
-    if let videoUrl = cardObject?.details?["videoUrl"].url {
-      playButton.isHidden = false
-      self.videoUrl = videoUrl
+    private func configurateByResponseObject(thumbnailUrl: String) {
+        if let videoUrl = cardObject?.details?["videoUrl"].url {
+            playButton.isHidden = false
+            self.videoUrl = videoUrl
+        }
+        if let videoPreviewImageUrl = cardObject?.details?[thumbnailUrl].url {
+            debugLog("Invitation Card - start load image")
+            invitationImageView.setLogs(enabled: true)
+            invitationImageView.loadImageData(with: videoPreviewImageUrl)
+        }
     }
-    if let videoPreviewImageUrl = cardObject?.details?[thumbnailUrl].url {
-      debugLog("Invitation Card - start load image")
-      invitationImageView.setLogs(enabled: true)
-      invitationImageView.loadImageData(with: videoPreviewImageUrl)
-    }
-  }
     
-  @IBAction func actionCloseButton(_ sender: Any) {
-    deleteCard()
-  }
+    @IBAction func actionCloseButton(_ sender: Any) {
+        deleteCard()
+    }
     
-  override func deleteCard() {
-    super.deleteCard()
-    CardsManager.default.stopOperationWith(type: viewType)
-    if viewType == .invitation {
-      analyticsService.trackCustomGAEvent(eventCategory: .functions, eventActions: .invitation, eventLabel: .homePageCard(.close))
-      AnalyticsService.sendNetmeraEvent(event: NetmeraEvents.Actions.HomepageCard(cardName: NetmeraEventValues.HomePageCardEventValue.invitation.text, action: NetmeraEventValues.HomePageCardEventValue.dismiss.text))
+    override func deleteCard() {
+        super.deleteCard()
+        CardsManager.default.stopOperationWith(type: viewType)
+        if viewType == .invitation {
+            analyticsService.trackCustomGAEvent(eventCategory: .functions, eventActions: .invitation, eventLabel: .homePageCard(.close))
+            AnalyticsService.sendNetmeraEvent(event: NetmeraEvents.Actions.HomepageCard(cardName: NetmeraEventValues.HomePageCardEventValue.invitation.text, action: NetmeraEventValues.HomePageCardEventValue.dismiss.text))
+        }
     }
-  }
     
-  @IBAction func playButtonTapped(_ sender: Any) {
-    guard let videoUrl = videoUrl else {
-      assertionFailure()
-      return
+    @IBAction func playButtonTapped(_ sender: Any) {
+        guard let videoUrl = videoUrl else {
+            assertionFailure()
+            return
+        }
+        
+        if viewType == .invitation {
+            analyticsService.trackCustomGAEvent(eventCategory: .videoAnalytics, eventActions: .startVideo, eventLabel: .invitation(.invitationVideoButton))
+        }
+        let player = AVPlayer(url: videoUrl)
+        let playerController = FixedAVPlayerViewController()
+        playerController.player = player
+        let nController = NavigationController(rootViewController: playerController)
+        nController.navigationBar.isHidden = true
+        RouterVC().presentViewController(controller: nController, animated: true) {
+            player.play()
+        }
     }
-      
-    if viewType == .invitation {
-      analyticsService.trackCustomGAEvent(eventCategory: .videoAnalytics, eventActions: .startVideo, eventLabel: .invitation(.invitationVideoButton))
-    }
-    let player = AVPlayer(url: videoUrl)
-    let playerController = FixedAVPlayerViewController()
-    playerController.player = player
-    let nController = NavigationController(rootViewController: playerController)
-    nController.navigationBar.isHidden = true
-    RouterVC().presentViewController(controller: nController, animated: true) {
-      player.play()
-    }
-  }
     
-  @IBAction func bottomButtonTapped(_ sender: Any) {
-    let router = RouterVC()
-    if viewType == .invitation {
-      analyticsService.trackCustomGAEvent(eventCategory: .functions, eventActions: .invitation, eventLabel: .homePageCard(.letsSee))
-      AnalyticsService.sendNetmeraEvent(event: NetmeraEvents.Actions.HomepageCard(cardName: NetmeraEventValues.HomePageCardEventValue.invitation.text, action: NetmeraEventValues.HomePageCardEventValue.detail.text))
-      let controller = router.invitationController()
-      router.pushViewController(viewController: controller)
-    } else if viewType == .paycell {
-      router.pushViewController(viewController: router.paycell)
+    @IBAction func bottomButtonTapped(_ sender: Any) {
+        let router = RouterVC()
+        if viewType == .invitation {
+            analyticsService.trackCustomGAEvent(eventCategory: .functions, eventActions: .invitation, eventLabel: .homePageCard(.letsSee))
+            AnalyticsService.sendNetmeraEvent(event: NetmeraEvents.Actions.HomepageCard(cardName: NetmeraEventValues.HomePageCardEventValue.invitation.text, action: NetmeraEventValues.HomePageCardEventValue.detail.text))
+            let controller = router.invitationController()
+            router.pushViewController(viewController: controller)
+        } else if viewType == .paycell {
+            router.pushViewController(viewController: router.paycell)
+        }
     }
-  }
 }
