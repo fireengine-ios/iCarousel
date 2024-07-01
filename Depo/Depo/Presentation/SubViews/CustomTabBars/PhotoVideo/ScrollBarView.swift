7a161f6eb5f7fdf18d7f0fa8022846cd530d3d3e
TURKCELL\EXT02D51886 <ozan.salman@consultant.turkcell.com.tr>
2024-07-01 10:51:06 +0300

    LB-2825 photos header freeze quick scroll bug fixed

--- Depo/Depo/Presentation/SubViews/CustomTabBars/PhotoVideo/ScrollBarView.swift
+++ Depo/Depo/Presentation/SubViews/CustomTabBars/PhotoVideo/ScrollBarView.swift
@@ -95,6 +95,7 @@ final class ScrollBarView: UIView {
     private let animationDuration = 0.3
     private let hideAnimationDelay = 1.0
     private var cellWidth: CGFloat = 100
+    private var headerHeight = 50.0
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -218,7 +219,7 @@ final class ScrollBarView: UIView {
         
         var frame = CGRect.zero
         frame.size.width = scrollBarWidth
-        frame.size.height = scrollViewFrame.height - (scrollView.adjustedContentInset.top + scrollView.adjustedContentInset.bottom) //isDragging ? originalHeight : height
+        frame.size.height = scrollViewFrame.height - (scrollView.adjustedContentInset.top + scrollView.adjustedContentInset.bottom) - headerHeight //isDragging ? originalHeight : height
         frame.origin.x = scrollViewFrame.width - (edgeInset + halfWidth) - scrollView.safeAreaInsets.right
         
         frame.origin.x = min(frame.origin.x, scrollViewFrame.width - scrollBarWidth)
@@ -231,7 +232,7 @@ final class ScrollBarView: UIView {
 //            frame.origin.y += largeTitleDelta
 //        }
         
-        frame.origin.y = scrollView.contentOffset.y + scrollView.adjustedContentInset.top
+        frame.origin.y = scrollView.contentOffset.y + scrollView.adjustedContentInset.top + headerHeight
         
         self.frame = frame
     }
